4a405aad750a82a21659365f6fb7c423ba74ac32
Nakamura <60052047+naakaamura@users.noreply.github.com>
2020-06-03 12:58:19 -0300

    Napalm

--- EEMod.cs
+++ EEMod.cs
@@ -4,5 +4,16 @@ namespace EEMod
 {
 	public class EEMod : Mod
 	{
+		public static EEMod instance;
+
+		public override void Load()
+        {
+            instance = this;
+        }
+
+		public override void Unload()
+        {
+            instance = null;  
+        }
 	}
 }
