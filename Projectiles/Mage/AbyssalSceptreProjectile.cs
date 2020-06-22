43e7d6d5500e0c7e23436117366073eedaf162e3
OSGAgaming <41522151+OSGAgaming@users.noreply.github.com>
2020-06-22 16:39:29 +0100

    updated abyssalScep

--- Projectiles/Mage/AbyssalSceptreProjectile.cs
+++ Projectiles/Mage/AbyssalSceptreProjectile.cs
@@ -1,8 +1,10 @@
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
+using System;
 
-namespace EEMod.Projectiles.Mage
+namespace InteritosMod.Projectiles.Mage
 {
     public class AbyssalSceptreProjectile : ModProjectile
     {
@@ -24,22 +26,40 @@ namespace EEMod.Projectiles.Mage
 
         public override void AI()
         {
+            if (isClone)
+            {
+                projectile.ai[0]++;
+                if(projectile.ai[0] == 1)
+                for (var a = 0; a < 50; a++)
+                {
+                    Vector2 vector = new Vector2(0, 20).RotatedBy(((Math.PI * 0.04) * a), default);
+                    int index = Dust.NewDust(projectile.Center, 22, 22, DustID.SolarFlare, vector.X, vector.Y, 0, new Color(0, 255, 0), 1f);
+                    Main.dust[index].velocity *= .5f;
+                    Main.dust[index].noGravity = true;
+                }
+            }
             Dust.NewDust(projectile.position, projectile.width, projectile.height, 75);
             projectile.rotation = projectile.velocity.ToRotation();
         }
 
-        public bool isClone;
+        private bool isClone;
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
+            Main.NewText("Yes");
             if (!isClone)
             {
                 float rot = Main.rand.NextFloat(6.28f);
-                Vector2 position = target.Center + Vector2.One.RotatedBy(rot) * 160;
-                for (int i = 0; i < 30; i++)
-                    Dust.NewDust(position, projectile.width, projectile.height, 75);
+                Vector2 position = target.Center + Vector2.One.RotatedBy(rot) * 300;
+                for (var a = 0; a < 50; a++)
+                {
+                    Vector2 vector = new Vector2(0, 20).RotatedBy(((Math.PI * 0.04) * a), default);
+                    int index = Dust.NewDust(projectile.Center, 22, 22, DustID.SolarFlare, vector.X, vector.Y, 0, new Color(0,255,0), 1f);
+                    Main.dust[index].velocity *= 1.1f;
+                    Main.dust[index].noGravity = true;
+                }
                 Vector2 velocity = Vector2.One.RotatedBy(rot) * -1 * 16f;
-                int newProj = Projectile.NewProjectile(position, velocity, ModContent.ProjectileType<AbyssalSceptreProjectile>(), projectile.damage, projectile.knockBack);
-                Main.projectile[newProj].tileCollide = false;
+                int newProj = Projectile.NewProjectile(position, velocity, ModContent.ProjectileType<AbyssalSceptreProjectile>(), damage, projectile.knockBack, projectile.owner, 0, 0);
+                Main.projectile[newProj].tileCollide = true;
                 Main.projectile[newProj].timeLeft = 60;
                 (Main.projectile[newProj].modProjectile as AbyssalSceptreProjectile).isClone = true;
             }
