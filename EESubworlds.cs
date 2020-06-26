cef5c40666759c4daea887c4c9a02ca3a9353d78
EpicCrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2020-06-26 15:44:42 -0700

    Fricking subworlds annoying, done with volcano surgery

--- EESubworlds.cs
+++ EESubworlds.cs
@@ -132,7 +132,7 @@ namespace EEMod
         }
         public static void VolcanoIsland(int seed, GenerationProgress customProgressObject = null)
         {
-            /*Main.maxTilesX = 400;
+            Main.maxTilesX = 400;
             Main.maxTilesY = 405;
             //Main.worldSurface = Main.maxTilesY;
             //Main.rockLayer = Main.maxTilesY;
@@ -157,43 +157,7 @@ namespace EEMod
             SubworldManager.SettleLiquids();
             EEMod.isSaving = false;
             Main.spawnTileX = 200;
-            Main.spawnTileY = 100;*/
-
-            //int depth = 70;
-            int boatPos = Main.maxTilesX / 2;
-            Main.maxTilesX = 1000;
-            Main.maxTilesY = 2000;
-            SubworldManager.Reset(seed);
-            SubworldManager.PostReset(customProgressObject);
-            /*EEWorld.EEWorld.FillRegion(Main.maxTilesX, Main.maxTilesY, new Vector2(0, 0), ModContent.TileType<HardenedGemsandTile>());
-            EEWorld.EEWorld.ClearRegion(Main.maxTilesX, Main.maxTilesY / 20, Vector2.Zero);
-            for (int i = 0; i < 10; i++)
-            {
-                for (int j = -5; j < 5; j++)
-                    WorldGen.TileRunner(300 + (i * 170) + (j * 10), Main.maxTilesY / 20, 10, 10, ModContent.TileType<HardenedGemsandTile>(), true, 0, 0, true, true);
-            }
-            for (int i = 0; i < 100; i++)
-            {
-                for (int j = -5; j < 5; j++)
-                    WorldGen.TileRunner(300 + (i * 17) + (j * 10), Main.maxTilesY / 20, 4, 10, ModContent.TileType<HardenedGemsandTile>(), true, 0, 0, true, true);
-            }
-            EEWorld.EEWorld.FillRegionNoEdit(Main.maxTilesX, Main.maxTilesY / 20, new Vector2(0, Main.maxTilesY / 40), ModContent.TileType<CoralSand>());
-            EEWorld.EEWorld.CoralReef();
-            for (int i = 2; i < Main.maxTilesX - 2; i++)
-            {
-                for (int j = 2; j < Main.maxTilesY - 2; j++)
-                {
-                    Tile.SmoothSlope(i, j);
-                }
-            }
-            EEWorld.EEWorld.KillWall(1000, 1000, Vector2.Zero);
-            EEWorld.EEWorld.FillRegionWithWater(Main.maxTilesX, Main.maxTilesY - depth, new Vector2(0, depth));
-            EEWorld.EEWorld.PlaceShip(boatPos, EEWorld.EEWorld.TileCheckWater(boatPos) - 22, EEWorld.EEWorld.ShipTiles);
-            EEWorld.EEWorld.PlaceShipWalls(boatPos, EEWorld.EEWorld.TileCheckWater(boatPos) - 22, EEWorld.EEWorld.ShipWalls);
-            CoralBoatPos = new Vector2(boatPos, EEWorld.EEWorld.TileCheckWater(boatPos) - 22);*/
-            EEMod.isSaving = false;
-            Main.spawnTileX = boatPos;
-            Main.spawnTileY = boatPos;
+            Main.spawnTileY = 100;
         }
         public static void VolcanoInside(int seed, GenerationProgress customProgressObject = null)
         {
