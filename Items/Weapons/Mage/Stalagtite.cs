615673fac9a68a2a4d997e94dee4b50c7e5eb6bb
LoLXD8783 <lelomlalol@gmail.com>
2020-06-29 00:29:09 -0500

    Code checc

--- Items/Weapons/Mage/Stalagtite.cs
+++ Items/Weapons/Mage/Stalagtite.cs
@@ -1,3 +1,4 @@
+using System;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -29,6 +30,7 @@ namespace EEMod.Items.Weapons.Mage
             item.height = 40;
             item.useStyle = ItemUseStyleID.HoldingOut;
 
+            item.shoot = ModContent.ProjectileType<StalagtiteProjectile>();
             item.shootSpeed = 9f;
             item.knockBack = 3.5f;
 
@@ -37,17 +39,11 @@ namespace EEMod.Items.Weapons.Mage
 
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
-            position += new Vector2(100, 100);
-            Vector2 target = Main.screenPosition + new Vector2(Main.mouseX, Main.mouseY);
-            float ceilingLimit = target.Y;
-            if (ceilingLimit > player.Center.Y - 200f)
-            {
-                ceilingLimit = player.Center.Y - 200f;
-            }
+            Vector2 target = Main.MouseWorld;
+            float ceilingLimit = Math.Min(target.Y, player.Center.Y - 200);
             for (int i = 0; i < 2; i++)
             {
-                position = player.Center + new Vector2(-(float)Main.rand.Next(0, 401) * player.direction, -600f);
-                position.Y -= 100 * i;
+                position = player.Center + new Vector2(-Main.rand.NextFloat(401) * player.direction, -(600f + (100 * i)));
                 Vector2 heading = target - position;
                 if (heading.Y < 0f)
                 {
@@ -60,7 +56,7 @@ namespace EEMod.Items.Weapons.Mage
                 heading.Normalize();
                 heading *= new Vector2(speedX, speedY).Length();
                 speedX = heading.X;
-                speedY = heading.Y + Main.rand.Next(-40, 41) * 0.02f;
+                speedY = heading.Y + Main.rand.NextFloat(-40/50f, 41/50f);
                 Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage * 2, knockBack, player.whoAmI, 0f, ceilingLimit);
             }
             return false;
