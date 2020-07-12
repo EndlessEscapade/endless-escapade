89f7f5c3fde819f6f45b95924d621ace7dd2a722
OSGAgaming <41522151+OSGAgaming@users.noreply.github.com>
2020-07-12 23:35:17 +0100

    Appropriate map Icons Yes

--- Tiles/AtlanteanBrickTile.cs
+++ Tiles/AtlanteanBrickTile.cs
@@ -13,7 +13,7 @@ namespace EEMod.Tiles
             Main.tileSolid[Type] = true;
             Main.tileBlendAll[Type] = true;
 
-            AddMapEntry(new Color(67, 47, 155));
+            AddMapEntry(new Color(68, 89, 195));
 
             dustType = 154;
             drop = ModContent.ItemType<AtlanteanBrick>();
