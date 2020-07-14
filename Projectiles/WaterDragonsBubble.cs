85a74e0014eb5011e679d3e62e49106cceb2575d
EpicCrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2020-07-14 00:29:18 -0700

    recommit

---
+++ Projectiles/WaterDragonsBubble.cs
@@ -0,0 +1,28 @@
+using Terraria;
+using Terraria.ModLoader;
+
+namespace EEMod.Projectiles
+{
+    public class WaterDragonsBubble : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Water Dragon's Bubble");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 24;
+            projectile.height = 24;
+            projectile.friendly = true;
+            projectile.timeLeft = 120;
+            projectile.hostile = false;
+        }
+
+        public override void AI()
+        {
+            if(projectile.velocity.Y <= 2)
+                projectile.velocity.Y *= 1.02f;
+        }
+    }
+}
