8221b4b89da9dc24dc9982fdd53c0ef903c2f67c
EpicCrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2020-07-15 13:17:44 -0700

    Akumo

---
+++ Items/Placeables/Furniture/Moyai.cs
@@ -0,0 +1,30 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using EEMod.Tiles.Furniture;
+
+namespace EEMod.Items.Placeables.Furniture
+{
+    public class Moyai : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Moyai");
+        }
+
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.maxStack = 1;
+            item.consumable = true;
+            item.width = 12;
+            item.height = 12;
+
+            item.createTile = ModContent.TileType<MoyaiTile>();
+        }
+    }
+}
