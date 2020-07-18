0267edbe6df4f1355ed5b93a3b23dd6c7031c444
EpicCrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2020-07-18 12:46:32 -0700

    ae

---
+++ Items/Placeables/Furniture/Trophies.cs
@@ -0,0 +1,116 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using EEMod.Tiles.Furniture;
+
+namespace EEMod.Items.Placeables.Furniture
+{
+    public class HydrosTrophy : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.maxStack = 1;
+            item.consumable = true;
+            item.width = 12;
+            item.height = 12;
+            item.rare = ItemRarityID.White;
+            item.createTile = ModContent.TileType<Trophies>();
+            item.placeStyle = 0;
+        }
+    }
+    public class OmenTrophy : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.maxStack = 1;
+            item.consumable = true;
+            item.width = 12;
+            item.height = 12;
+            item.rare = ItemRarityID.White;
+            item.createTile = ModContent.TileType<Trophies>();
+            item.placeStyle = 1;
+        }
+    }
+    public class AkumoTrophy : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.maxStack = 1;
+            item.consumable = true;
+            item.width = 12;
+            item.height = 12;
+            item.rare = ItemRarityID.White;
+            item.createTile = ModContent.TileType<Trophies>();
+            item.placeStyle = 2;
+        }
+    }
+    public class KrakenTrophy : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.maxStack = 1;
+            item.consumable = true;
+            item.width = 12;
+            item.height = 12;
+            item.rare = ItemRarityID.White;
+            item.createTile = ModContent.TileType<Trophies>();
+            item.placeStyle = 3;
+        }
+    }
+    public class TalosTrophy : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.maxStack = 1;
+            item.consumable = true;
+            item.width = 12;
+            item.height = 12;
+            item.rare = ItemRarityID.White;
+            item.createTile = ModContent.TileType<Trophies>();
+            item.placeStyle = 4;
+        }
+    }
+    public class CoralGolemTrophy : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.useStyle = 1;
+            item.useTurn = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.autoReuse = true;
+            item.maxStack = 1;
+            item.consumable = true;
+            item.width = 12;
+            item.height = 12;
+            item.rare = ItemRarityID.White;
+            item.createTile = ModContent.TileType<Trophies>();
+            item.placeStyle = 5;
+        }
+    }
+}
---
+++ Tiles/Furniture/TrophiesTile.cs
@@ -0,0 +1,55 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using EEMod.Items.Placeables.Furniture.Trophies;
+
+namespace EEMod.Tiles.Furniture
+{
+    public class Trophies : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true; // Necessary since Style3x3Wall uses AnchorWall
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3Wall);
+            TileObjectData.newTile.StyleHorizontal = true;
+            TileObjectData.newTile.StyleWrapLimit = 36;
+            TileObjectData.addTile(Type);
+            dustType = 7;
+            disableSmartCursor = true;
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Trophy");
+            AddMapEntry(new Color(120, 85, 60), name);
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            int item = 0;
+            switch (frameX / 54)
+            {
+                case 0:
+                    item = ModContent.ItemType<HydrosTrophy>();
+                    break;
+                case 1:
+                    item = ModContent.ItemType<OmenTrophy>();
+                    break;
+                case 2:
+                    item = ModContent.ItemType<AkumoTrophy>();
+                    break;
+                case 3:
+                    item = ModContent.ItemType<KrakenTrophy>();
+                    break;
+                case 4:
+                    item = ModContent.ItemType<TalosTrophy>();
+                    break;
+                case 5:
+                    item = ModContent.ItemType<CoralGolemTrophy>();
+                    break;
+            }
+            Item.NewItem(i * 16, j * 16, 48, 48, item);
+        }
+    }
+}
