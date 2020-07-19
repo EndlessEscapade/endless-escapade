9f0fc2666783afc135d81a4cbd4251df41218305
EpicCrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2020-07-19 11:12:54 -0700

    Misc sumbissions stuff

---
+++ Items/Food/Cheese.cs
@@ -0,0 +1,35 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EEMod.Items.Food
+{
+    public class Cheese : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Cheese");
+            Tooltip.SetDefault("'Enjoy it while you can'");
+            ItemID.Sets.SortingPriorityMaterials[item.type] = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 50;
+            item.height = 34;
+            item.maxStack = 999;
+            item.value = Item.buyPrice(0, 1, 0, 0);
+            item.rare = ItemRarityID.Blue;
+            item.useAnimation = 12;
+            item.useTime = 12;
+            item.useStyle = ItemUseStyleID.EatingUsing;
+            item.consumable = true;
+            item.UseSound = SoundID.Item2;
+        }
+        public override bool UseItem(Player player)
+        {
+            player.AddBuff(BuffID.WellFed, 90000);
+            return true;
+        }
+    }
+}
---
+++ Projectiles/Volleyball.cs
@@ -0,0 +1,15 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace EEMod.Projectiles
+{
+    public class Volleyball : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(ProjectileID.BeachBall);
+        }
+    }
+}
