9c55fca8529735236400f793ceb36fcc4c3d6984
EpicCrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2020-07-23 19:56:54 -0700

    Above

---
+++ Items/Tools/Hydrofluoric/HydrofluoricHamaxe.cs
@@ -0,0 +1,42 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using EEMod.Items.Placeables.Ores;
+
+namespace EEMod.Items.Tools.Dalantinium
+{
+    public class HydrofluoricHamaxe : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Hydrofluoric Hamaxe");
+        }
+
+        public override void SetDefaults()
+        {
+            item.axe = 13;
+            item.hammer = 50;
+            item.useTime = 38;
+            item.useAnimation = 38;
+            item.width = 20;
+            item.height = 20;
+            item.rare = ItemRarityID.Orange;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.value = Item.sellPrice(0, 0, 48);
+            item.damage = 12;
+            item.melee = true;
+            item.autoReuse = true;
+            item.UseSound = SoundID.Item1;
+            item.knockBack = 3f;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ModContent.ItemType<HydroFluoricBar>(), 12);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
---
+++ Items/Tools/Hydrofluoric/HydrofluoricPiakxe.cs
@@ -0,0 +1,41 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using EEMod.Items.Placeables.Ores;
+
+namespace EEMod.Items.Tools.Dalantinium
+{
+    public class HydrofluoricPickaxe : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Hydrofluoric Pickaxe");
+        }
+
+        public override void SetDefaults()
+        {
+            item.melee = true;
+            item.pick = 70;
+            item.useTime = 19;
+            item.useAnimation = 19;
+            item.damage = 7;
+            item.rare = ItemRarityID.Orange;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.width = 16;
+            item.height = 16;
+            item.value = Item.sellPrice(0, 0, 21);
+            item.knockBack = 2f;
+            item.autoReuse = true;
+            item.UseSound = SoundID.Item1;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ModContent.ItemType<HydroFluoricBar>(), 12);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
