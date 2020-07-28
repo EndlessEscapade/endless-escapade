4b393ec5d22bf640a05f7e468728a998fbd5f4aa
EpicCrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2020-07-28 10:59:40 -0700

    ae

---
+++ Items/Weapons/Melee/FlareSmasher.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using EEMod.Items.Placeables.Ores;
+using EEMod.Projectiles.Melee;
+
+namespace EEMod.Items.Weapons.Melee
+{
+    public class FlareSmasher : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Flare Smasher");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 20;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.useAnimation = 70;
+            item.useTime = 24;
+            item.shootSpeed = 4;
+            item.knockBack = 6.5f;
+            item.width = 32;
+            item.height = 32;
+            item.scale = 1f;
+            item.rare = ItemRarityID.Purple;
+            item.value = Item.sellPrice(silver: 10);
+
+            item.melee = true;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+            item.autoReuse = true;
+
+            item.UseSound = SoundID.Item1;
+            item.shoot = ModContent.ProjectileType<FlareSmasherProj>();
+        }
+    }
+}
