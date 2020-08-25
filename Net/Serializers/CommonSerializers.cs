45acca26d2c52aafeb9960f8a7a66b652045eb59
LoLXD8783 <lelomlalol@gmail.com>
2020-08-25 14:28:53 -0500

    Move net serializers to 1 file

---
+++ Net/Serializers/CommonSerializers.cs
@@ -0,0 +1,138 @@
+﻿using System.IO;
+using Microsoft.Xna.Framework;
+
+namespace EEMod.Net.Serializers
+{
+    public class ByteSerializer : NetObjSerializer<byte>
+    {
+        public override byte Read(BinaryReader reader) => reader.ReadByte();
+        public override void Write(BinaryWriter writer, byte value) => writer.Write(value);
+        public override SerializerPriority Priority => SerializerPriority.Low;
+    }
+
+    public class SByteSerializer : NetObjSerializer<sbyte>
+    {
+        public override sbyte Read(BinaryReader reader) => reader.ReadSByte();
+        public override void Write(BinaryWriter writer, sbyte value) => writer.Write(value);
+        public override SerializerPriority Priority => SerializerPriority.Low;
+    }
+
+    public class Int16Serializer : NetObjSerializer<short>
+    {
+        public override short Read(BinaryReader reader) => reader.ReadInt16();
+        public override void Write(BinaryWriter writer, short value) => writer.Write(value);
+        public override SerializerPriority Priority => SerializerPriority.Low;
+    }
+
+    public class UInt16Serializer : NetObjSerializer<ushort>
+    {
+        public override ushort Read(BinaryReader reader) => reader.ReadUInt16();
+        public override void Write(BinaryWriter writer, ushort value) => writer.Write(value);
+        public override SerializerPriority Priority => SerializerPriority.Low;
+    }
+
+    public class Int32Serializer : NetObjSerializer<int>
+    {
+        public override int Read(BinaryReader reader) => reader.ReadInt32();
+        public override void Write(BinaryWriter writer, int value) => writer.Write(value);
+        public override SerializerPriority Priority => SerializerPriority.Low;
+    }
+
+    public class UInt32Serializer : NetObjSerializer<uint>
+    {
+        public override uint Read(BinaryReader reader) => reader.ReadUInt32();
+        public override void Write(BinaryWriter writer, uint value) => writer.Write(value);
+        public override SerializerPriority Priority => SerializerPriority.Low;
+    }
+
+    public class Int64Serializer : NetObjSerializer<long>
+    {
+        public override long Read(BinaryReader reader) => reader.ReadInt64();
+        public override void Write(BinaryWriter writer, long value) => writer.Write(value);
+        public override SerializerPriority Priority => SerializerPriority.Low;
+    }
+
+    public class UInt64Serializer : NetObjSerializer<ulong>
+    {
+        public override ulong Read(BinaryReader reader) => reader.ReadUInt64();
+        public override void Write(BinaryWriter writer, ulong value) => writer.Write(value);
+        public override SerializerPriority Priority => SerializerPriority.Low;
+    }
+
+    public class SingleSerializer : NetObjSerializer<float>
+    {
+        public override float Read(BinaryReader reader) => reader.ReadSingle();
+        public override void Write(BinaryWriter writer, float value) => writer.Write(value);
+        public override SerializerPriority Priority => SerializerPriority.Low;
+    }
+
+    public class DoubleSerializer : NetObjSerializer<double>
+    {
+        public override double Read(BinaryReader reader) => reader.ReadDouble();
+        public override void Write(BinaryWriter writer, double value) => writer.Write(value);
+        public override SerializerPriority Priority => SerializerPriority.Low;
+    }
+
+    public class CharSerializer : NetObjSerializer<char>
+    {
+        public override char Read(BinaryReader reader) => reader.ReadChar();
+        public override void Write(BinaryWriter writer, char value) => writer.Write(value);
+        public override SerializerPriority Priority => SerializerPriority.Low;
+    }
+
+    public class DecimalSerializer : NetObjSerializer<decimal>
+    {
+        public override decimal Read(BinaryReader reader) => reader.ReadDecimal();
+        public override void Write(BinaryWriter writer, decimal value) => writer.Write(value);
+        public override SerializerPriority Priority => SerializerPriority.Low;
+    }
+
+    public class Vector2Serializer : NetObjSerializer<Vector2>
+    {
+        public override Vector2 Read(BinaryReader reader) => new Vector2(reader.ReadSingle(), reader.ReadSingle());
+
+        public override void Write(BinaryWriter writer, Vector2 value)
+        {
+            writer.Write(value.X);
+            writer.Write(value.Y);
+        }
+    }
+
+    public class Vector3Serializer : NetObjSerializer<Vector3>
+    {
+        public override Vector3 Read(BinaryReader reader) => new Vector3(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
+
+        public override void Write(BinaryWriter writer, Vector3 value)
+        {
+            writer.Write(value.X);
+            writer.Write(value.Y);
+            writer.Write(value.Z);
+        }
+    }
+
+    public class Vector4Serializer : NetObjSerializer<Vector4>
+    {
+        public override Vector4 Read(BinaryReader reader) => new Vector4(reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle(), reader.ReadSingle());
+
+        public override void Write(BinaryWriter writer, Vector4 value)
+        {
+            writer.Write(value.X);
+            writer.Write(value.Y);
+            writer.Write(value.Z);
+            writer.Write(value.W);
+        }
+    }
+
+    public class RectangleSerializer : NetObjSerializer<Rectangle>
+    {
+        public override Rectangle Read(BinaryReader reader) => new Rectangle(reader.ReadInt32(), reader.ReadInt32(), reader.ReadInt32(), reader.ReadInt32());
+
+        public override void Write(BinaryWriter writer, Rectangle value)
+        {
+            writer.Write(value.X);
+            writer.Write(value.Y);
+            writer.Write(value.Width);
+            writer.Write(value.Height);
+        }
+    }
+}
--- Net/Serializers/SerializersManager.cs
+++ Net/Serializers/SerializersManager.cs
@@ -9,29 +9,6 @@ namespace EEMod.Net.Serializers
 {
     public class SerializersManager : AutoloadTypeManager<NetObjSerializer>
     {
-        class SerializerInfo
-        {
-            LinkedList<Tuple<SerializerPriority, NetObjSerializer>> serializers;
-            NetObjSerializer cachedHighest;
-            public NetObjSerializer GetHighestPrioritySerializer() => cachedHighest;
-            public void AddSerializer(SerializerPriority priority, NetObjSerializer serializer)
-            {
-                Tuple<SerializerPriority, NetObjSerializer> m = Tuple.Create(priority, serializer);
-                serializers.AddLast(m);
-                foreach (var s in serializers)
-                {
-                    if (s.Item1 > m.Item1)
-                        m = s;
-                }
-                cachedHighest = m.Item2;
-            }
-            public SerializerInfo(NetObjSerializer defaultSerializer, SerializerPriority priority = SerializerPriority.Medium)
-            {
-                serializers = new LinkedList<Tuple<SerializerPriority, NetObjSerializer>>();
-                serializers.AddFirst(Tuple.Create(priority, cachedHighest = defaultSerializer));
-            }
-        }
-
         [FieldInit]
         private static ConcurrentDictionary<Type, SerializerInfo> serializers = new ConcurrentDictionary<Type, SerializerInfo>();
 
@@ -39,11 +16,10 @@ namespace EEMod.Net.Serializers
         {
             if (type.CouldBeInstantiated() && type.IsSubclassOfGeneric(typeof(NetObjSerializer<>), out Type typ))
             {
-                Type serializingTargetType = typ.GenericTypeArguments[0];
+                Type serializingTargetType = typ.GenericTypeArguments[0]; // NetObjSerializer<thetype>
                 if (type.TryCreateInstance(out NetObjSerializer serializer))
                     AddSerializer(serializingTargetType, serializer, serializer.Priority);
             }
-
         }
 
         public static void AddSerializer(Type fortype, NetObjSerializer serializer, SerializerPriority priority = SerializerPriority.Medium)
@@ -58,5 +34,31 @@ namespace EEMod.Net.Serializers
 
         public static NetObjSerializer GetTypeSerializer(Type fortype) => serializers.TryGetValue(fortype, out var serializer) ? serializer.GetHighestPrioritySerializer() : null;
         public static NetObjSerializer<T> GetTypeSerializer<T>() => (NetObjSerializer<T>)GetTypeSerializer(typeof(T));
+
+        class SerializerInfo
+        {
+            Dictionary<SerializerPriority, NetObjSerializer> serializers;
+            private NetObjSerializer _cachedHighest;
+            public NetObjSerializer GetHighestPrioritySerializer() => _cachedHighest;
+            public void AddSerializer(SerializerPriority priority, NetObjSerializer serializer)
+            {
+                serializers.Add(priority, serializer);
+                foreach(var s in serializers)
+                {
+                    if(s.Key > priority)
+                    {
+                        priority = s.Key;
+                        serializer = s.Value;
+                    }
+                }
+                _cachedHighest = serializer;
+            }
+            public SerializerInfo(NetObjSerializer defaultSerializer, SerializerPriority priority = SerializerPriority.Medium)
+            {
+                serializers = new Dictionary<SerializerPriority, NetObjSerializer>();
+                serializers.Add(priority, defaultSerializer);
+                _cachedHighest = defaultSerializer;
+            }
+        }
     }
 }
