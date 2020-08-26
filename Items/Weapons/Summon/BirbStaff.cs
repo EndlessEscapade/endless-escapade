866617ef1cb29397976d9ba2035231a5b1ae716d
The-A44 <44125509+The-A44@users.noreply.github.com>
2020-08-26 09:21:32 -0400

    Isn't he so cute

--- Items/Weapons/Summon/BirbStaff.cs
+++ Items/Weapons/Summon/BirbStaff.cs
@@ -12,6 +12,7 @@ namespace EEMod.Items.Weapons.Summon
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Birb Staff");
+            Tooltip.SetDefault("Isn't he so cute.");
         }
 
         public override void SetDefaults()
@@ -21,16 +22,21 @@ namespace EEMod.Items.Weapons.Summon
             item.noMelee = true;
             item.autoReuse = true;
             item.value = Item.sellPrice(0, 0, 18);
-            item.damage = 13;
+            item.damage = 36;
             item.useTime = 26;
             item.useAnimation = 26;
             item.width = 20;
             item.height = 20;
             item.rare = ItemRarityID.Green;
             item.knockBack = 5f;
-            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.useStyle = 1;
             item.UseSound = SoundID.Item8;
-            //item.shoot = ModContent.ProjectileType<ARProj>();
+            item.shoot = ModContent.ProjectileType<AkumoMinion>();
         }
+
+        /*public override bool CanUseItem(Player player)
+        {
+            return !Main.LocalPlayer.HasBuff(ModContent.BuffType<AkumoBuff>());
+        }*/
     }
 }
