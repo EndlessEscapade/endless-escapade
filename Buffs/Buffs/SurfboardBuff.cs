51cf8426b27f03b87406abd1cb2b4195075f2ef1
EpicCrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2020-09-05 13:58:27 -0700

    surfboard base

---
+++ Buffs/Buffs/SurfboardBuff.cs
@@ -0,0 +1,23 @@
+using Terraria;
+using Terraria.ModLoader;
+using EEMod.Mounts;
+
+namespace EEMod.Buffs.Buffs
+{
+    public class SurfboardBuff : ModBuff
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Surfboard");
+            Description.SetDefault("Surf's up, duuuude!");
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = true;
+        }
+
+        public override void Update(Player player, ref int buffIndex)
+        {
+            player.mount.SetMount(ModContent.MountType<Mounts.Surfboard>(), player);
+            player.buffTime[buffIndex] = 10;
+        }
+    }
+}
