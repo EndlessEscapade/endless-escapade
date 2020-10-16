3b0c8f7ef8de86e872f1e8e156d61078d7f81722
EpicCrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2020-10-15 21:14:44 -0700

    im bumb

--- EEPlayer.cs
+++ EEPlayer.cs
@@ -1244,5 +1244,192 @@ namespace EEMod
                 hours++;
             }
         }
+
+        internal static void UpdateOceanMapElements()
+        {
+            for (int i = 0; i < OceanMapElements.Count; i++)
+            {
+                OceanMapElements[i].Update();
+            }
+        }
+
+        public struct Island
+        {
+            EEPlayer player => Main.LocalPlayer.GetModPlayer<EEPlayer>();
+            public Island(Vector2 pos, Texture2D tex, string NameOfIsland, int frameCount = 1, int frameSpid = 2, bool canCollide = false, int startingFrame = 0)
+            {
+                posX = (int)pos.X;
+                posY = (int)pos.Y;
+                texture = tex;
+                frames = frameCount;
+                frameSpeed = frameSpid;
+                currentFrame = startingFrame;
+                this.canCollide = canCollide;
+                if (NameOfIsland != null)
+                {
+                    if (!player.Islands.ContainsKey(NameOfIsland))
+                    {
+                        player.Islands.Add(NameOfIsland, this);
+                    }
+                }
+            }
+
+            private readonly int posX;
+            private readonly int posY;
+            public int frames;
+            public int currentFrame;
+            public int frameSpeed;
+            public bool canCollide;
+
+            public int posXToScreen
+            {
+                get => posX + (int)Main.screenPosition.X + Main.screenWidth;
+            }
+
+            public int posYToScreen
+            {
+                get => posY + (int)Main.screenPosition.Y + Main.screenHeight;
+            }
+
+            public Texture2D texture;
+            public Vector2 posToScreen => new Vector2(posXToScreen - texture.Width / 2, posYToScreen - texture.Height / (2 * frames));
+            public Rectangle hitBox => new Rectangle((int)posToScreen.X, (int)posToScreen.Y - texture.Height / (frames * 2), texture.Width, texture.Height / (frames));
+            private Rectangle ShipHitBox => new Rectangle((int)Main.screenPosition.X + (int)EEMod.instance.position.X - 30, (int)Main.screenPosition.Y + (int)EEMod.instance.position.Y - 30, 30, 30);
+            public bool isColliding => hitBox.Intersects(ShipHitBox) && canCollide;
+        }
+
+        public class DarkCloud : IOceanMapElement
+        {
+            public Vector2 pos;
+            public float flash;
+
+            public int posXToScreen
+            {
+                get => (int)(pos.X + Main.screenPosition.X);
+            }
+
+            public int posYToScreen
+            {
+                get => (int)(pos.Y + Main.screenPosition.Y);
+            }
+
+            public Texture2D texture;
+            public float scale, alpha;
+            private readonly EEPlayer modPlayer = Main.LocalPlayer.GetModPlayer<EEPlayer>();
+
+            public DarkCloud(Vector2 pos, Texture2D tex, float scale, float alpha)
+            {
+                flash += 0.01f;
+                this.pos = pos;
+                texture = tex;
+                this.scale = scale;
+                this.alpha = alpha;
+                flash = Main.rand.NextFloat(0, 4);
+            }
+
+            public void Draw(SpriteBatch spriteBatch)
+            {
+                flash += 0.003f;
+                Vector2 p = new Vector2(posXToScreen + (float)Math.Sin(flash) * 10, posYToScreen - 1000).ForDraw();
+                Color drawcolor = Lighting.GetColor(posXToScreen / 16, (posYToScreen - 1000) / 16);
+                drawcolor.A = (byte)alpha;
+                if (modPlayer.quickOpeningFloat > 0.01f)
+                {
+                    float lerp = 1 - (modPlayer.quickOpeningFloat / 10f);
+                    spriteBatch.Draw(texture, p, null, drawcolor * lerp, 0f, default, scale, SpriteEffects.None, 0f);
+                    return;
+                }
+                spriteBatch.Draw(texture, p, null, drawcolor * (1 - (modPlayer.cutSceneTriggerTimer / 180f)), 0f, default, scale, SpriteEffects.None, 0f);
+            }
+
+            public void Update()
+            {
+
+            }
+        }
+
+        public class MovingCloud : IOceanMapElement
+        {
+            public Vector2 pos;
+            public float flash;
+
+            public int posXToScreen
+            {
+                get => (int)(pos.X + Main.screenPosition.X);
+            }
+
+            public int posYToScreen
+            {
+                get => (int)(pos.Y + Main.screenPosition.Y);
+            }
+
+            public Texture2D texture;
+            public float scale, alpha;
+            private readonly EEPlayer modPlayer = Main.LocalPlayer.GetModPlayer<EEPlayer>();
+
+            public MovingCloud(Vector2 pos, Texture2D tex, float scale, float alpha)
+            {
+                this.pos = pos;
+                texture = tex;
+                this.scale = scale;
+                this.alpha = alpha;
+                flash = Main.rand.NextFloat(0, 4);
+            }
+
+            public void Draw(SpriteBatch spriteBatch)
+            {
+                flash += 0.003f;
+                Vector2 p = new Vector2(posXToScreen + (float)Math.Sin(flash) * 10, posYToScreen - 1000).ForDraw();
+                Color drawcolor = Lighting.GetColor(posXToScreen / 16, (posYToScreen - 1000) / 16);
+                drawcolor.A = (byte)alpha;
+                spriteBatch.Draw(texture, p, null, drawcolor * (1 - (modPlayer.cutSceneTriggerTimer / 180f)), 0f, default, scale, SpriteEffects.None, 0f);
+            }
+
+            public void Update()
+            {
+            }
+        }
+
+        public class MCloud : IOceanMapElement
+        {
+            private Vector2 position;
+            private readonly int width, height;
+            private readonly float alpha, scale;
+            private readonly Texture2D texture;
+            private readonly EEPlayer modPlayer = Main.LocalPlayer.GetModPlayer<EEPlayer>();
+            private Vector2 Center => new Vector2(position.X + width / 2f, position.Y + height / 2f);
+
+            public MCloud(Texture2D texture, Vector2 position, int width, int height, float scale, float alpha)
+            {
+                //scale = projectile.ai[0];
+                //alpha = (int)projectile.ai[1];
+                this.scale = scale;
+                this.alpha = alpha;
+                this.texture = texture;
+                this.position = position;
+                this.width = width;
+                this.height = height;
+            }
+
+            public void Draw(SpriteBatch spriteBatch)
+            {
+                Vector2 newPos = Center + Main.screenPosition;
+                Rectangle rect = new Rectangle(0, 0, width, height);
+                Color lightColour = Lighting.GetColor((int)newPos.X / 16, (int)newPos.Y / 16);
+                spriteBatch.Draw(texture, Center, rect, lightColour * ((255 - alpha) / 255f) * (1 - (modPlayer.cutSceneTriggerTimer / 180f)), 0f, rect.Size() / 2, scale, SpriteEffects.None, 0f);
+            }
+
+            public void Update()
+            {
+                position.X -= 0.3f;
+            }
+        }
+
+        internal interface IOceanMapElement
+        {
+            void Update();
+
+            void Draw(SpriteBatch spriteBatch);
+        }
     }
 }
