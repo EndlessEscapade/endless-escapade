ef02a77d037193918538125ee645683000f11093
OSGAgaming <41522151+OSGAgaming@users.noreply.github.com>
2020-10-20 17:53:08 +0100

    agggg

--- Tiles/EmptyTile.cs
+++ Tiles/EmptyTile.cs
@@ -19,12 +19,12 @@ namespace EEMod.Tiles
             dustType = 154;
             drop = ModContent.ItemType<CoralSand>();
             soundStyle = 1;
-            mineResist = 1f;
-            minPick = 0;
+            mineResist = 4f;
+            minPick = 100;
         }
         public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
         {
-            EmptyTileEntityCache.Invoke(new Vector2(i, j));
+          EmptyTileEntityCache.Invoke(new Vector2(i, j));
         }
         public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
         {
