a3b194957947e0b410cda1d2a0806f01f35a252b
OSGAgaming <41522151+OSGAgaming@users.noreply.github.com>
2020-11-15 03:28:09 +0000

    more grass

--- Tiles/LightGemsandstoneTile.cs
+++ Tiles/LightGemsandstoneTile.cs
@@ -30,18 +30,20 @@ namespace EEMod.Tiles
             Color drawColour = Lighting.GetColor(i, j);
             if (!Main.tileSolid[Framing.GetTileSafely(i, j - 1).type] || !Framing.GetTileSafely(i, j - 1).active() && Framing.GetTileSafely(i, j).slope() == 0)
             {
-                int noOfGrassBlades = (int)(((i + j) % 16) * 0.5f);
+                int noOfGrassBlades = (int)(((i + j) % 16) * 0.4f);
                 Texture2D tex = EEMod.instance.GetTexture("Tiles/KelpGrassLong");
-                Texture2D tex2 = EEMod.instance.GetTexture("Tiles/KelpGrassMedium");
                 Texture2D tex3 = EEMod.instance.GetTexture("Tiles/KelpGrassShort");
                 Texture2D tex4 = EEMod.instance.GetTexture("Tiles/KelpGrassStubbed");
+                Texture2D tex5 = EEMod.instance.GetTexture("Tiles/KelpGrassLongX");
+                Texture2D tex6 = EEMod.instance.GetTexture("Tiles/KelpGrassLongXX");
+                Texture2D tex2 = EEMod.instance.GetTexture("Tiles/KelpGrassMedium");
                 Texture2D Chosen = tex;
                 for (int a = 0; a < noOfGrassBlades; a++)
                 {
-                    float sprout = (float)Math.Sin(Main.time / 60f + i + j + a) * 0.2f;
+                    float sprout = (float)Math.Sin(Main.time / (60f - (i + j + a * 1.3f)%16) + i + j + a*1.3f) * 0.2f;
 
-                    bool cond = ((i + j * a * 3) % 2 == 0);
-                    switch ((i + j + a * 3) % 4)
+                    bool cond = ((i + j * a * 2) % 2 == 0);
+                    switch ((i + j + a * 3) % 6)
                     {
                         case 0:
                             Chosen = tex;
@@ -55,9 +57,57 @@ namespace EEMod.Tiles
                         case 3:
                             Chosen = tex4;
                             break;
+                        case 4:
+                            Chosen = tex5;
+                            break;
+                        case 5:
+                            Chosen = tex6;
+                            break;
                     }
-                    Vector2 position = new Vector2((i + 12) * 16 + (i + j * a + a * 4) % 16, (j + 12) * 16 + 2).ForDraw() - new Vector2((cond ? 0 : Chosen.Width), 0);
+                    Vector2 position = new Vector2((i + 12) * 16 + (i + j * a + a * 7) % 16, (j + 12) * 16 + 2).ForDraw() - new Vector2((cond ? 0 : Chosen.Width), 0);
                     spriteBatch.Draw(Chosen, position, Chosen.Bounds, drawColour.MultiplyRGB(Color.Lerp(Color.LightGreen, Color.Green, ((i + j + a * 3) % 4) / 4f)), sprout, new Vector2(0, Chosen.Height), 1f, cond ? SpriteEffects.None : SpriteEffects.FlipHorizontally, 0f);
+
+
+                }
+            }
+            if (!Main.tileSolid[Framing.GetTileSafely(i, j + 1).type] || !Framing.GetTileSafely(i, j + 1).active() && Framing.GetTileSafely(i, j).slope() == 0)
+            {
+                int noOfGrassBlades = (int)(((i + j) % 16) * 0.5f);
+                Texture2D tex = EEMod.instance.GetTexture("Tiles/KelpGrassLong");
+                Texture2D tex3 = EEMod.instance.GetTexture("Tiles/KelpGrassShort");
+                Texture2D tex4 = EEMod.instance.GetTexture("Tiles/KelpGrassStubbed");
+                Texture2D tex5 = EEMod.instance.GetTexture("Tiles/KelpGrassLongX");
+                Texture2D tex6 = EEMod.instance.GetTexture("Tiles/KelpGrassLongXX");
+                Texture2D tex2 = EEMod.instance.GetTexture("Tiles/KelpGrassMedium");
+                Texture2D Chosen = tex;
+                for (int a = 0; a < noOfGrassBlades; a++)
+                {
+                    float sprout = (float)Math.Sin(Main.time / (60f - (i + j + a * 1.3f) % 16) + i + j + a * 1.3f) * 0.2f;
+
+                    bool cond = ((i + j * a * 7) % 2 == 0);
+                    switch ((i + j + a * 7) % 6)
+                    {
+                        case 0:
+                            Chosen = tex;
+                            break;
+                        case 1:
+                            Chosen = tex2;
+                            break;
+                        case 2:
+                            Chosen = tex3;
+                            break;
+                        case 3:
+                            Chosen = tex4;
+                            break;
+                        case 4:
+                            Chosen = tex5;
+                            break;
+                        case 5:
+                            Chosen = tex6;
+                            break;
+                    }
+                    Vector2 position = new Vector2((i + 12) * 16 + (i + j * a + a * 3) % 16, (j + 12) * 16 + 14).ForDraw() - new Vector2((cond ? 0 : Chosen.Width), 0);
+                    spriteBatch.Draw(Chosen, position, Chosen.Bounds, drawColour.MultiplyRGB(Color.Lerp(Color.LightGreen, Color.Green, ((i + j + a * 7) % 4) / 4f)), sprout, new Vector2(0, 0), 1f, (cond ? SpriteEffects.None : SpriteEffects.FlipHorizontally) | SpriteEffects.FlipVertically, 0f);
 
 
                 }
