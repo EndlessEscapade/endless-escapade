042af75630e5e19ecfa55994c38fad16bf10a2a5
OSGAgaming <41522151+OSGAgaming@users.noreply.github.com>
2020-12-02 11:58:34 +0000

    removed obselete random bool

--- EEMod.Detours.cs
+++ EEMod.Detours.cs
@@ -78,12 +78,11 @@ namespace EEMod
         {
             orig(self, spriteBatch, drawOffset, waterStyle, globalAlpha, isBackgroundDraw);
         }*/
-        bool randombool;
         private void Main_DrawPlayer(On.Terraria.Main.orig_DrawPlayer orig, Main self, Player drawPlayer, Vector2 Position, float rotation, Vector2 rotationOrigin, float shadow)
         {
-           // if(!randombool)
-             orig(self, drawPlayer, Position, rotation, rotationOrigin, shadow);
-            if (randombool)
+            // if(!Main.LocalPlayer.GetModPlayer<EEPlayer>().isLight)
+            orig(self, drawPlayer, Position, rotation, rotationOrigin, shadow);
+            if (Main.LocalPlayer.GetModPlayer<EEPlayer>().isLight)
             {
                /* Particles.Get("Main").SetSpawningModules(new SpawnRandomly(0.004f));
                 Particles.Get("Main").SpawnParticles(Main.LocalPlayer.Center, null, ModContent.GetInstance<EEMod>().GetTexture("Particles/Cross"), 200, 1, null, new CircularMotionSinSpin(3, 15, 0.06f, Main.LocalPlayer, 0, 0f, 0.04f, 0.01f));
