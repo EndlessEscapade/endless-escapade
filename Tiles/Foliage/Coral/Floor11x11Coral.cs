371f3d92bccfa0572f4e4f800d41b3628c72f395
EpicCrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2020-12-25 16:20:45 -0800

    Vadim corals

---
+++ Tiles/Foliage/Coral/Floor11x11Coral.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Enums;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace EEMod.Tiles.Foliage.Coral
+{
+    public class Floor11x11Coral : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolidTop[Type] = false;
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileLavaDeath[Type] = false;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style6x3);
+            TileObjectData.newTile.Width = 11;
+            TileObjectData.newTile.Height = 11;
+            TileObjectData.newTile.Origin = new Point16(0, 0);
+            TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16 };
+            TileObjectData.newTile.CoordinateWidth = 16;
+            TileObjectData.newTile.CoordinatePadding = 2;
+            TileObjectData.newTile.Direction = TileObjectDirection.None;
+            TileObjectData.newTile.LavaDeath = false;
+            TileObjectData.addTile(Type);
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Big Coral");
+            AddMapEntry(new Color(20, 60, 20), name);
+            disableSmartCursor = true;
+            dustType = 107;
+        }
+    }
+}
