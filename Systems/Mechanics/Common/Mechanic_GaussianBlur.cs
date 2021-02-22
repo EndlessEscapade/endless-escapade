1a4a71748bbb112a651216e29843c87c764b6abf
EpicCrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2021-02-21 16:10:28 -0800

    added gaussian blur mechanic(temp)

---
+++ Systems/Mechanics/Common/Mechanic_GaussianBlur.cs
@@ -0,0 +1,30 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace EEMod
+{
+    public class GaussianBlur : Mechanic
+    {
+        public override void OnDraw()
+        {
+            base.OnDraw();
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+        }
+
+        public override void OnLoad()
+        {
+            base.OnLoad();
+        }
+        protected override Layer DrawLayering => base.DrawLayering;
+    }
+}
