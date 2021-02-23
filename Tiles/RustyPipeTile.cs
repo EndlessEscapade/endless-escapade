8d2595b8cbab23b0e9bc4fef18e168d815a35be0
EpicCrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2021-02-23 07:24:10 -0800

    rusty pipe

---
+++ Tiles/RustyPipeTile.cs
@@ -0,0 +1,30 @@
+using EEMod.Items.Placeables;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace EEMod.Tiles
+{
+    public class RustyPipeTile : ModTile
+    {
+        /*public override void ChangeWaterfallStyle(ref int style)
+        {
+            style = mod.GetWaterfallStyleSlot("Surfacebg");
+        }*/
+
+        public override void SetDefaults()
+        {
+            Main.tileMergeDirt[Type] = false;
+            Main.tileSolid[Type] = true;
+            Main.tileBlendAll[Type] = true;
+
+            AddMapEntry(new Color(48, 115, 135));
+
+            dustType = 154;
+            drop = ModContent.ItemType<RustyPipe>();
+            soundStyle = 1;
+            mineResist = 1f;
+            minPick = 0;
+        }
+    }
+}
