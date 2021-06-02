2f09acdb6148de7abc9e4ecf1432d310c004cce4
Stevie <xxlennygamerxx@gmail.com>
2021-06-01 18:59:40 -0700

    duplicate flag property

--- Systems/Structurizer/PlacementActions/IRepeatedPlacementAction.cs
+++ Systems/Structurizer/PlacementActions/IRepeatedPlacementAction.cs
@@ -3,8 +3,6 @@
     public interface IRepeatedPlacementAction<out TPlacementAction> : IPlacementAction
         where TPlacementAction : IPlacementAction
     {
-        ushort Flag { get; }
-
         ushort RepetitionCount { get; }
 
         TPlacementAction PlacementAction { get; }
