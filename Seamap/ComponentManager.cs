67b7cdb2202b1eda22a5981e8aab3b7b3480dcfd
LoLXD8783 <lelomlalol@gmail.com>
2022-02-12 15:59:42 -0600

    rewrite component manager

--- Seamap/ComponentManager.cs
+++ Seamap/ComponentManager.cs
@@ -1,25 +1,21 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using Terraria;
+using Terraria.ModLoader;
+
+#nullable enable
 
 namespace EEMod.Seamap.SeamapContent
 {
-    public class ComponentManager // implementation might change to use static arrays
+    public class ComponentManager
     {
-        Dictionary<Type, object> components = new();
-        // Ref<T> are used because struct components
-        // prevents having to box/unbox (cast to object and back to T)
-        // and allow to get a reference to the component
+        public SeamapObject Parent { get; }
 
-        /// <summary>
-        /// The object these components belong to.
-        /// </summary>
-        public SeamapObject Parent { get; protected set; }
-
-        /// <summary>
-        /// All the current components registered.
-        /// </summary>
-        public IEnumerable<Type> Components => components.Keys;
+        public Dictionary<Type, object> Components { get; } = new();
 
         public ComponentManager(SeamapObject parent)
         {
@@ -27,137 +23,42 @@ namespace EEMod.Seamap.SeamapContent
         }
 
         /// <summary>
-        /// Sets or adds a component of type <typeparamref name="T"/>.
+        /// Gets a component
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="value">The value of the component.</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public void Set<T>(T value)
+        /// <returns>The component, or <see langword="null"/> if its not present</returns>
+        public T Get<T>() where T : Component 
+            => Components.TryGetValue(typeof(T), out var component) ? (T)component : null!;
+
+        public T Get<T>(bool throwsIfNotPresent) where T : Component
         {
-            if (value == null)
-                throw new ArgumentNullException(nameof(value));
+            if(Components.TryGetValue(typeof(T), out var entry))
+                return (T)entry;
+            
+            if (throwsIfNotPresent)
+                throw new KeyNotFoundException($"Component of type {typeof(T).FullName} was not found");
 
-            if (typeof(T) != value.GetType())
-                throw new ArgumentException("typeof(T) != value.GetType()");
-
-            components[value.GetType()] = new Ref<T>(value);
+            return null!;
         }
 
-        /// <summary>
-        /// Sets or adds a component of type <typeparamref name="T"/>.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="wrapper">The wrapper for the component</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public void Set<T>(Ref<T> wrapper)
+        public bool TryGet<T>([MaybeNullWhen(false)] out T component) where T : Component
         {
-            if (wrapper == null)
-                throw new ArgumentNullException(nameof(wrapper));
-
-            components[typeof(T)] = wrapper; 
-        }
-
-        /// <summary>
-        /// Removes a component of type <typeparamref name="T"/>.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <returns>If the component was succesfully removed</returns>
-        public bool Remove<T>()
-        {
-            return components.Remove(typeof(T));
-        }
-
-        /// <summary>
-        /// Attempts to get the <seealso cref="Ref{T}"/> wrapper of a component of type <typeparamref name="T"/>.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="component">The component wrapper.</param>
-        /// <returns>If the component was successfully taken.</returns>
-        public bool TryGet<T>(out Ref<T> component)
-        {
-            if(!components.TryGetValue(typeof(T), out object obj))
+            if(Components.TryGetValue(typeof(T), out var entry))
             {
-                component = null;
-                return false;
+                component = (T)entry;
+                return true;
             }
-
-            component = (Ref<T>)obj;
-            return true;
+            component = null;
+            return false;
         }
 
-        /// <summary>
-        /// Attempts to get the reference of a component of type <typeparamref name="T"/>.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="success">If the component was successfully taken.</param>
-        /// <param name="throwIfNotFound">Throw if the component is not present.</param>
-        /// <returns>
-        /// The reference to the component. If the component is not found and <paramref name="throwIfNotFound"/> is false<br />
-        /// returns <seealso cref="RefNull{T}._null"/>.
-        /// </returns>
-        /// <exception cref="InvalidOperationException"></exception>
-        public ref T GetRef<T>(out bool success, bool throwIfNotFound = true)
-        {
-            if(!components.TryGetValue(typeof(T), out object obj))
-            {
-                if(throwIfNotFound)
-                    throw new InvalidOperationException($"No component of type {typeof(T)} is registered");
-                success = false;
-                return ref RefNull<T>._null;
-            }
+        public bool Has<T>() where T : Component
+            => Components.ContainsKey(typeof(T));
 
-            success = true;
-            return ref ((Ref<T>)obj).Value;
-        }
+        public bool Remove<T>() where T : Component
+            => Components.Remove(typeof(T));
 
-        /// <summary>
-        /// Attempts to get the <see cref="Ref{T}"/> wrapper of a component of type <paramref name="componentType"/>
-        /// </summary>
-        /// <param name="componentType">The type of the component.</param>
-        /// <param name="componentWrapper">The <see cref="Ref{T}"/> wrapper as <see cref="object"/></param>
-        /// <returns>If the component exists and was succesfully taken.</returns>
-        public bool TryGet(Type componentType, out object componentWrapper)
-        {
-            return components.TryGetValue(componentType, out componentWrapper);
-        }
-
-        /// <summary>
-        /// If there's a component of type <typeparamref name="T"/>.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <returns></returns>
-        public bool Has<T>()
-        {
-            return components.ContainsKey(typeof(T));
-        }
-
-        /// <summary>
-        /// If there's a component of type <paramref name="componentType"/>.
-        /// </summary>
-        /// <param name="componentType">The type of the component.</param>
-        /// <returns></returns>
-        public bool Has(Type componentType)
-        {
-            return components.ContainsKey(componentType);
-        }
+        public IEnumerable<T> ComponentsOfType<T>() where T : Component
+            => Components.Values.OfType<T>();
     }
 }
-
-        //public ref T Get<T>()
-        //{
-        //    if (!components.TryGetValue(typeof(T), out var obj))
-        //        throw new InvalidOperationException($"No component of type {typeof(T)} is registered");
-        //    return ref ((Ref<T>)obj).Value;
-        //}
-
-        //public ref T Get<T>(out bool success)
-        //{
-        //    if (!components.TryGetValue(typeof(T), out var obj))
-        //    {
-        //        success = false;
-        //        return ref RefNull<T>._null;
-        //    }
-        //    success = true;
-        //    return ref ((Ref<T>)obj).Value;
-        //}
-
