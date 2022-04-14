8ad901a0dcd540a3596c3c32309704277b128c46
EpicCrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2022-04-14 09:56:15 -0700

    this brawnch is screwed

---
+++ ID/KeyID.cs
@@ -0,0 +1,16 @@
+using Terraria;
+using Terraria.GameInput;
+using Terraria.Graphics.Effects;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+namespace EEMod.ID
+{
+    public class KeyID
+    {
+        public const string Sea = "Sea";
+        public const string CoralReefs = "CoralReefs";
+        public const string Island = "Island";
+        public const string GoblinFort = "GoblinFort";
+    }
+}
