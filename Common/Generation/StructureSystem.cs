fcecb6370c1be211c6677661666ee33c3b560b53
naakaamura <joaovtnakamura@gmail.com>
2023-02-19 14:07:09 -0300

    Beach structures + Localization entries

---
+++ Common/Generation/StructureSystem.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework.Input;
+using StructureHelper;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EndlessEscapade.Common.Generation;
+
+public sealed class StructureSystem : ModSystem
+{
+    public override void PostUpdateEverything() {
+        if (Main.keyState.IsKeyDown(Keys.F) && !Main.oldKeyState.IsKeyDown(Keys.F)) {
+            PlacePier((int)(Main.MouseWorld.X / 16f), (int)(Main.MouseWorld.Y / 16f));
+        }
+    }
+
+    private void PlacePier(int i, int j) {
+        Generator.GenerateStructure("Assets/Structures/Pier", new Point16(i, j), Mod);
+
+        ExtendPillar(5, 24);
+        ExtendPillar(6, 24);
+        
+        ExtendPillar(21, 24);
+        ExtendPillar(22, 24);
+        
+        ExtendPillar(37, 24);
+        ExtendPillar(38, 24);
+        
+        void ExtendPillar(int xOffset, int yOffset) {
+            int pillarX = i + xOffset;
+            int pillarY = j + yOffset;
+
+            while (!WorldGen.SolidTile(pillarX, pillarY)) {
+                WorldGen.PlaceTile(pillarX, pillarY, TileID.LivingWood, true, true);
+                WorldGen.SlopeTile(pillarX, pillarY, (int)SlopeType.Solid);
+
+                pillarY++;
+            }
+        }
+    }
+}
---
+++ Content/Tiles/Reefs/Thermal/BasaltTile.cs
@@ -0,0 +1,31 @@
+﻿using EndlessEscapade.Common.Tiles;
+using EndlessEscapade.Content.Items.Reefs.Thermal;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EndlessEscapade.Content.Tiles.Reefs.Thermal;
+
+public class BasaltTile : ModTile
+{
+    public override void SetStaticDefaults() {
+        Main.tileMergeDirt[Type] = false;
+        Main.tileSolid[Type] = true;
+        Main.tileLighted[Type] = true;
+        Main.tileBlockLight[Type] = true;
+        
+        TileID.Sets.Conversion.Stone[Type] = true;
+
+        AddMapEntry(new Color(37, 45, 45));
+
+        MineResist = 1f;
+        
+        HitSound = SoundID.Tink;
+        DustType = DustID.Ash;
+    }
+
+    public override void NumDust(int i, int j, bool fail, ref int num) {
+        num = fail ? 1 : 3;
+    }
+}
