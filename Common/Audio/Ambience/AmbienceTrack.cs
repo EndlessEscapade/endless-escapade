623cabc138396256835af19f63fd8c9cae989e44
LoLXD8783 <56768047+LoLXD8783@users.noreply.github.com>
2023-02-22 19:22:49 -0600

    basic ambience playing

--- Common/Audio/Ambience/AmbienceTrack.cs
+++ Common/Audio/Ambience/AmbienceTrack.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using ReLogic.Content;
+using ReLogic.Utilities;
 using Terraria;
 using Terraria.Audio;
 using Terraria.ModLoader;
@@ -12,6 +13,7 @@ public abstract class AmbienceTrack : ModType
 {
     protected readonly List<AmbienceSoundEntry> soundEntries = new();
 
+    private SoundStyle soundStyle;
     // TODO: Change to use multiple SoundStyle s (Layered ambience).
     private SoundEffectInstance soundInstance;
 
@@ -22,10 +24,13 @@ public abstract class AmbienceTrack : ModType
     public virtual float FadeIn { get; } = 0.005f;
     public virtual float FadeOut { get; } = 0.005f;
 
+    SlotId soundSlot;
     public virtual void Setup() {
-        soundInstance = ModContent.Request<SoundEffect>(Path, AssetRequestMode.ImmediateLoad).Value.CreateInstance();
-        soundInstance.IsLooped = true;
-        soundInstance.Volume = 0f;
+        soundStyle = new(Path, SoundType.Ambient) {
+            PlayOnlyIfFocused = true,
+            IsLooped = true
+        };
+        soundSlot = SoundEngine.PlaySound(null);
     }
 
     public virtual void Update() {
@@ -42,30 +47,66 @@ public abstract class AmbienceTrack : ModType
         soundEntries.Add(entry);
     }
 
-    private void PlayMainLoop() {
-        if (soundInstance == null) {
-            return;
+    bool TryGetActiveSound(out ActiveSound result, bool tryPlaying = false) {
+        if (!soundSlot.IsValid || !SoundEngine.TryGetActiveSound(soundSlot, out result) && tryPlaying) {
+            TryReplay();
+            return SoundEngine.TryGetActiveSound(soundSlot, out result);
         }
-        
+        return result != null;
+    }
+    bool TryReplay() {
+        if (soundSlot.IsValid && SoundEngine.TryGetActiveSound(soundSlot, out ActiveSound activeSound)) { // theres an existing sound instance
+            SoundEffectInstance s = activeSound.Sound;
+            if (s != null && s.State != SoundState.Stopped) {
+                activeSound.Stop();
+            }
+        }
+        soundSlot = SoundEngine.PlaySound(in soundStyle);
+        Main.NewText("played new sound");
+        return soundSlot.IsValid;
+    }
+    float volumeScale;
+    private void PlayMainLoop() {
+
         if (Active) {
-            soundInstance.Volume += FadeIn;
+            volumeScale += FadeIn;
         }
         else if (!Active) {
-            soundInstance.Volume -= FadeOut;
+            volumeScale -= FadeOut;
         }
 
-        soundInstance.Volume = MathHelper.Clamp(soundInstance.Volume, 0f, Main.ambientVolume);
+        volumeScale = MathHelper.Clamp(volumeScale, 0f, 1f);
 
-        if (Main.hasFocus && !Main.gameMenu) {
-            if (soundInstance.State == SoundState.Stopped) {
-                soundInstance.Play();
+        float minVolume = 0;
+        bool valid = TryGetActiveSound(out ActiveSound activeSound, tryPlaying: false);
+        if (volumeScale <= minVolume) {
+            // try to pause, nothing to hear
+            if (valid) {
+                activeSound.Volume *= 0;
+                if (activeSound.IsPlaying)
+                    activeSound.Pause();
             }
-            else if (soundInstance.State == SoundState.Paused) {
-                soundInstance.Resume();
-            }
+            return;
         }
-        else {
-            soundInstance.Pause();
+        else { // can still play
+            if (Main.hasFocus && !Main.gameMenu) {
+                if (!valid || !activeSound.IsPlaying && activeSound.Sound?.State is null or SoundState.Stopped) { // sound finished 
+                    valid = TryGetActiveSound(out activeSound, tryPlaying: true);
+                }
+                else if (activeSound.Sound.State == SoundState.Paused) { // not finished but just paused
+                    activeSound.Resume();
+                }
+            }
+            else {
+                if (activeSound.IsPlaying) {
+                    activeSound.Pause();
+                }
+            }
+
+            if (valid && activeSound.IsPlaying) {
+                activeSound.Update();
+                activeSound.Sound.Volume *= volumeScale;
+            }
         }
     }
 
@@ -73,7 +114,7 @@ public abstract class AmbienceTrack : ModType
         if (!Active) {
             return;
         }
-    
+
         foreach (AmbienceSoundEntry entry in soundEntries) {
             if (!Main.rand.NextBool(entry.PlaybackChance)) {
                 continue;
