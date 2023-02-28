b7cc1b61343264e330a5a8408aca53a4f68a2e96
Req <justreqofficial@gmail.com>
2023-02-27 23:52:47 -0500

    pagination kinda works? stutters

--- Common/FishermansLogUI/FishermansLogGrid.cs
+++ Common/FishermansLogUI/FishermansLogGrid.cs
@@ -1,6 +1,8 @@
 using EndlessEscapade.Utilities.Extensions;
 using Microsoft.Xna.Framework;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
 using Terraria.ModLoader.UI.Elements;
 using Terraria.UI;
 
@@ -8,6 +10,8 @@ namespace EndlessEscapade.Common.FishermansLogUI;
 
 internal class FishermansLogGrid : UIGrid
 {
+    private float ElementSize;
+
     public FishermansLogGrid(float padding, float elementSize, int elementsPerRow, int elementsPerColumn, List<int> elements) : base() {
         HAlign = 0.5f;
         VAlign = 0.5f;
@@ -17,12 +21,24 @@ internal class FishermansLogGrid : UIGrid
         ListPadding = padding;
         SetPadding(padding);
 
+        ElementSize = elementSize;
+
+        PopulateGrid(elements);
+    }
+
+    private void PopulateGrid(List<int> elements) {
         elements.ForEach(i => {
-            FishermansLogGridElement Button = new FishermansLogGridElement(StyleDimension.FromPixels(elementSize), i).With(e => {
+            FishermansLogGridElement Button = new FishermansLogGridElement(StyleDimension.FromPixels(ElementSize), i).With(e => {
                 e.BorderColor = Color.Black * 0.25f;
             });
 
             Add(Button);
         });
+    }
+
+    public void SwitchElements(List<int> newElements) {
+        Clear();
+
+        PopulateGrid(newElements);
     }
 }
--- Common/FishermansLogUI/FishermansLogUIState.cs
+++ Common/FishermansLogUI/FishermansLogUIState.cs
@@ -9,6 +9,7 @@ using Terraria.ID;
 using System.Linq;
 using System;
 using Microsoft.Xna.Framework;
+using Terraria.Audio;
 
 namespace EndlessEscapade.Common.FishermansLogUI;
 
@@ -25,7 +26,6 @@ public class FishermansLogUIState : UIState
     }
 
     public static bool Visible => !Main.playerInventory;
-
     private readonly string texturePath = "EndlessEscapade/Assets/UI";
 
     readonly List<int> catches = new List<int>() { ItemID.ArmoredCavefish, ItemID.AtlanticCod, ItemID.Bass, ItemID.BlueJellyfish, ItemID.ChaosFish, ItemID.CrimsonTigerfish, ItemID.Damselfish, ItemID.DoubleCod, ItemID.Ebonkoi, ItemID.FlarefinKoi, ItemID.Flounder, ItemID.FrostMinnow, ItemID.GoldenCarp, ItemID.GreenJellyfish, ItemID.Hemopiranha, ItemID.Honeyfin, ItemID.NeonTetra, ItemID.Obsidifish, ItemID.PinkJellyfish, ItemID.PrincessFish, ItemID.Prismite, ItemID.RedSnapper, ItemID.RockLobster, ItemID.Salmon, ItemID.Shrimp, ItemID.SpecularFish, ItemID.Stinkfish, ItemID.Trout, ItemID.Tuna, ItemID.VariegatedLardfish };
@@ -33,6 +33,8 @@ public class FishermansLogUIState : UIState
     readonly List<int> questCatches = new List<int>() { ItemID.AmanitaFungifin, ItemID.Angelfish, ItemID.Batfish, ItemID.BloodyManowar, ItemID.Bonefish, ItemID.BumblebeeTuna, ItemID.Bunnyfish, ItemID.CapnTunabeard, ItemID.Catfish, ItemID.Cloudfish, ItemID.Clownfish, ItemID.Cursedfish, ItemID.DemonicHellfish, ItemID.Derpfish, ItemID.Dirtfish, ItemID.DynamiteFish, ItemID.EaterofPlankton, ItemID.FallenStarfish, ItemID.TheFishofCthulu, ItemID.Fishotron, ItemID.Fishron, ItemID.GuideVoodooFish, ItemID.Harpyfish, ItemID.Hungerfish, ItemID.Ichorfish, ItemID.InfectedScabbardfish, ItemID.Jewelfish, ItemID.MirageFish, ItemID.Mudfish, ItemID.MutantFlinxfin, ItemID.Pengfish, ItemID.Pixiefish, ItemID.ScarabFish, ItemID.ScorpioFish, ItemID.Slimefish, ItemID.Spiderfish, ItemID.TropicalBarracuda, ItemID.TundraTrout, ItemID.UnicornFish, ItemID.Wyverntail, ItemID.ZombieFish };
 
     int currentPage = 0;
+    List<List<int>> pages;
+    List<FishermansLogGrid> grids = new();
 
     public override void OnInitialize() {
         List<int> allCatches = catches.Concat(questCatches).OrderBy(i => new Item(i).Name).ToList();
@@ -64,9 +66,8 @@ public class FishermansLogUIState : UIState
                 e.BorderColor = Color.Transparent;
             }));
 
-            float padding = 15f; // Fuck around with these values
-            int elementsPerRow = 5; // and see what looks good ig
-
+            float padding = 15f;
+            int elementsPerRow = 5;
             float elementSize = (GridContainer.GetOuterDimensions().Width - ((elementsPerRow + 1) * padding)) / elementsPerRow;
 
             GridContainer.Height = StyleDimension.FromPixelsAndPercent(-(elementSize + padding), 1f);
@@ -74,9 +75,11 @@ public class FishermansLogUIState : UIState
 
             int elementsPerColumn = (int)((GridContainer.GetOuterDimensions().Height - padding) / (elementSize + padding));
 
-            List<List<int>> individualPages = SplitList(allCatches, elementsPerColumn * elementsPerRow);
+            pages = SplitList(allCatches, elementsPerColumn * elementsPerRow);
+            pages.Add(new());
 
-            FishermansLogGrid Grid = GridContainer.AddElement(new FishermansLogGrid(padding, elementSize, elementsPerRow, elementsPerColumn, individualPages[currentPage + i]));
+            FishermansLogGrid Grid = GridContainer.AddElement(new FishermansLogGrid(padding, elementSize, elementsPerRow, elementsPerColumn, pages[currentPage + i]));
+            grids.Add(Grid);
 
             UIHoverImageButton paginateButton = MainPanel.AddElement(new UIHoverImageButton(Main.Assets.Request<Texture2D>("Images/UI/Bestiary/Button_" + (i == 0 ? "Back" : "Forward"), ReLogic.Content.AssetRequestMode.ImmediateLoad), (i == 0 ? "Previous" : "Next") + " Page").With(e => {
                 e.Left = StyleDimension.FromPixels(padding * (i == 0 ? 1 : -1));
@@ -86,9 +89,25 @@ public class FishermansLogUIState : UIState
 
                 e.SetHoverImage(Main.Assets.Request<Texture2D>("Images/UI/Bestiary/Button_Border"));
                 e.SetVisibility(1f, 1f);
-                /*e.OnMouseOver += paginateButton_OnMouseOver;
-                e.OnClick += paginateButton_OnClick;*/
+                e.OnMouseOver += paginateButton_OnMouseOver;
+                e.OnClick += paginateButton_OnClick;
             }));
+        }
+    }
+
+    private void paginateButton_OnMouseOver(UIMouseEvent evt, UIElement listeningElement) {
+        SoundEngine.PlaySound(SoundID.MenuTick);
+    }
+
+    private void paginateButton_OnClick(UIMouseEvent evt, UIElement listeningElement) {
+        SoundEngine.PlaySound(SoundID.MenuTick);
+        UIHoverImageButton target = (UIHoverImageButton)listeningElement;
+
+        if ((target.HoverText.StartsWith("Previous") && currentPage == 0) || (target.HoverText.StartsWith("Next") && currentPage == pages.Count / 2)) return;
+
+        currentPage += 2 * (target.HoverText.StartsWith("Previous") ? -1 : 1);
+        for (int i = 0; i < grids.Count; i++) {
+            grids[i].SwitchElements(pages[currentPage + i]);
         }
     }
 }
