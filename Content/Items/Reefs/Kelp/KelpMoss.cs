612f0032f4abc7a1c2c8295307c01f3a6e4458cc
naakaamura <joaovtnakamura@gmail.com>
2023-04-15 19:42:36 -0300

    Custom moss placement

--- Content/Items/Reefs/Kelp/KelpMoss.cs
+++ Content/Items/Reefs/Kelp/KelpMoss.cs
@@ -1,4 +1,6 @@
-﻿using Terraria.ModLoader;
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace EndlessEscapade.Content.Items.Reefs.Kelp;
 
@@ -6,5 +8,33 @@ public class KelpMoss : ModItem
 {
     public override void SetDefaults() {
         Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Reefs.Kelp.KelpMoss>());
+        Item.createTile = -1;
+    }
+
+    public override void HoldItem(Player player) {
+        if (IsHoveringRock()) {
+            player.cursorItemIconEnabled = true;
+        }
+    }
+
+    public override bool? UseItem(Player player) {
+        if (Main.netMode == NetmodeID.Server) {
+            return false;
+        }
+
+        if (IsHoveringRock()) {
+            WorldGen.PlaceTile(Player.tileTargetX, Player.tileTargetY, ModContent.TileType<Tiles.Reefs.Kelp.KelpMoss>(), false, true);
+            
+            player.cursorItemIconEnabled = true;
+            player.inventory[player.selectedItem].stack--;
+        }
+
+        return true;
+    }
+
+    private static bool IsHoveringRock() {
+        var tile = Framing.GetTileSafely(Player.tileTargetX, Player.tileTargetY);
+
+        return tile.HasTile && tile.TileType == ModContent.TileType<Tiles.Reefs.Kelp.KelpRock>();
     }
 }
