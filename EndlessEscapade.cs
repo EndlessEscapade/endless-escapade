859fff9e6e4c90da4880cfc8cb981f4e43eadaf9
LoLXD8783 <56768047+LoLXD8783@users.noreply.github.com>
2023-05-07 19:31:47 -0600

    Execute effect compiler before compilation

--- EndlessEscapade.cs
+++ EndlessEscapade.cs
@@ -2,4 +2,7 @@
 
 namespace EndlessEscapade;
 
-public class EndlessEscapade : Mod { }
+public class EndlessEscapade : Mod
+{
+    public static EndlessEscapade Instance => ModContent.GetInstance<EndlessEscapade>();
+}
