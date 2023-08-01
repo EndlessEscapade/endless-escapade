f881fbb4b221efdcee2063e8ff5a652964b508fa
shnakamura <joaovtnakamura@gmail.com>
2023-08-01 15:16:01 -0300

    Custom titles

---
+++ Common/TitleSystem.cs
@@ -0,0 +1,22 @@
+﻿using EndlessEscapade.Utilities.Extensions;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace EndlessEscapade.Common;
+
+[Autoload(Side = ModSide.Client)]
+public class TitleSystem : ModSystem
+{
+    public const int TitleCount = 11;
+
+    public override void PostSetupContent() {
+        var selected = Mod.GetLocalizationValue($"Titles.Title{Main.rand.Next(TitleCount)}");
+        var title = $"{Mod.DisplayName}: {selected}";
+        
+        Main.instance.Window.Title = title;
+    }
+
+    public override void OnModUnload() {
+        Main.changeTheTitle = true;
+    }
+}
