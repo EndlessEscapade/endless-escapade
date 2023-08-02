c2674184f58e3207184dd22cd2932cca652cda52
nakamura <60052047+shnakamura@users.noreply.github.com>
2023-08-02 00:51:20 -0300

    Docs

--- Utilities/Extensions/ModExtensions.cs
+++ Utilities/Extensions/ModExtensions.cs
@@ -5,6 +5,7 @@ namespace EndlessEscapade.Utilities.Extensions;
 
 public static class ModExtensions
 {
+    /// <summary>Shorthand for <c>Language.GetTextValue($"Mods.{mod.Name}.{key}", args)</c>.</summary>
     public static string GetLocalizationValue(this Mod mod, string key) {
         return Language.GetTextValue($"Mods.{mod.Name}.{key}");
     }
