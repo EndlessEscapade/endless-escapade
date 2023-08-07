b4fc5ed950284f78f07e97c8d559a32aee737f6d
shnakamura <joaovtnakamura@gmail.com>
2023-08-07 01:30:09 -0300

    More ungenerated structures

---
+++ Common/Systems/Generation/OceanSystem.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.ModLoader;
+
+namespace EndlessEscapade.Common.Systems.Generation;
+
+public class OceanSystem : ModSystem
+{
+    public override void PostWorldGen() {
+        base.PostWorldGen();
+    }
+}
