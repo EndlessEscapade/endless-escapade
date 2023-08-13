199b9fbf0a6943bedb34519005451216370ada8f
nakamura <60052047+shnakamura@users.noreply.github.com>
2023-08-13 02:00:41 -0300

    useless

--- EndlessEscapade.cs
+++ EndlessEscapade.cs
@@ -5,12 +5,4 @@ namespace EndlessEscapade;
 public class EndlessEscapade : Mod
 {
     public static EndlessEscapade Instance => ModContent.GetInstance<EndlessEscapade>();
-
-    public override void Load() 
-    {
-        if (1 == 0)
-        {
-            
-        }
-    }
 }
