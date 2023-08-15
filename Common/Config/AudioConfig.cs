03fa6439a16dff35a1e4ce532b19353d2a4c4055
shnakamura <joaovtnakamura@gmail.com>
2023-08-15 16:32:26 -0300

    Audio configuration

---
+++ Common/Config/AudioConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace EndlessEscapade.Common.Config;
+
+public class AudioConfig : ModConfig
+{
+    public override ConfigScope Mode => ConfigScope.ClientSide;
+
+    [DefaultValue(true)]
+    [Header($"$Mods.{nameof(EndlessEscapade)}.Config.Audio.Header")]
+    [LabelKey($"$Mods.{nameof(EndlessEscapade)}.Config.Audio.{nameof(EnableLowPassFiltering)}.Label")]
+    [TooltipKey($"$Mods.{nameof(EndlessEscapade)}.Config.Audio.{nameof(EnableLowPassFiltering)}.Tooltip")]
+    public bool EnableLowPassFiltering { get; set; } = true;
+}
