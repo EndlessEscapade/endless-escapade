6d3f73a4d2b9952a80739eca09a69910626d875d
CrownKing <52501627+EpicCrownKing@users.noreply.github.com>
2023-08-20 15:08:16 -0700

    cryo cannon

---
+++ Content/Items/Shipyard/CryoCannon.cs
@@ -0,0 +1,10 @@
+﻿using Terraria.ModLoader;
+
+namespace EndlessEscapade.Content.Items.Shipyard;
+
+public class CryoCannon : ModItem
+{
+    public override void SetDefaults() {
+        Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.Shipyard.CryoCannon>());
+    }
+}
