98e5202f2eef07454f1fe9216a1f7f9ed4a823c9
shnakamura <joaovtnakamura@gmail.com>
2023-08-20 20:15:16 -0300

    fuck yall

--- Common/Systems/Net/MessageHandlerAttribute.cs
+++ Common/Systems/Net/MessageHandlerAttribute.cs
@@ -1,16 +1,7 @@
 ﻿using System;
 
-namespace EndlessEscapade.Common.Systems.Net;
-
-/// <summary>Static methods with this attribute are automatically registered to <see cref="Message{T}.OnRecieve" />.</summary>
-/// <remarks>The method must return void and contain a single parameter for the packet (<c>static void AnyName(T anyName)</c>)</remarks>
-[AttributeUsage(AttributeTargets.Method, Inherited = false)]
-public class MessageHandlerAttribute : Attribute
+namespace EndlessEscapade.Common.Systems.Net
 {
-<<<<<<< HEAD
-    public MessageHandlerAttribute(Type targetMessageType) {
-        TargetMessageType = targetMessageType;
-=======
     /// <summary>Static methods with this attribute are automatically registered to <see cref="MessageInfo{T}.OnRecieve"/>.</summary>
     /// <remarks>The method must return void and contain a single parameter for the packet (<c>static void AnyName(T anyName)</c>)</remarks>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
@@ -20,8 +11,5 @@ public class MessageHandlerAttribute : Attribute
         public MessageHandlerAttribute(Type targetMessageType) {
             TargetMessageType = targetMessageType;
         }
->>>>>>> 16c34049c6f66190f06d86ba3d55a46a5cc023b0
     }
-
-    public Type TargetMessageType { get; }
 }
--- Common/Systems/Net/NetSystem.cs
+++ Common/Systems/Net/NetSystem.cs
@@ -1,84 +1,77 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 using Terraria.ModLoader;
 using Terraria.ModLoader.Core;
+#nullable enable
 
-namespace EndlessEscapade.Common.Systems.Net;
-
-public class NetSystem : ModSystem
+namespace EndlessEscapade.Common.Systems.Net
 {
-    private static readonly Dictionary<int, IMessageHandler> netMessagesHandlers = new Dictionary<int, IMessageHandler>();
-    
-    public static int MessageCount { get; internal set; }
+    public class NetSystem : ModSystem
+    {
+        public static int MessageCount { get; internal set; }
+        private static Dictionary<int, IMessageHandler> netMessagesHandlers = new();
 
-<<<<<<< HEAD
-    /// <summary>Returns the assigned ID for the specified packet or 0 if the packet has not been registered.</summary>
-    /// <typeparam name="T"></typeparam>
-    /// <returns>The net ID of the packet.</returns>
-    public static int PacketID<T>() where T : INetMessage<T> {
-        return Message<T>.ID;
-    }
-    
-    internal static void HandleModRecievedPacket(BinaryReader reader, int fromWho) {
-        int messageID = reader.ReadByte();
-        if (netMessagesHandlers.TryGetValue(messageID, out var value)) {
-            value.Handle(reader, fromWho);
-=======
         /// <summary>Returns the assigned ID for the specified packet or 0 if the packet has not been registered.</summary>
         /// <typeparam name="T"></typeparam>
         /// <returns>The net ID of the packet.</returns>
         public static int PacketID<T>() where T : INetMessage<T> {
             return MessageInfo<T>.ID;
->>>>>>> 16c34049c6f66190f06d86ba3d55a46a5cc023b0
         }
-        else {
-            EndlessEscapade.Instance.Logger.Warn($"A message of type {messageID} has been recieved but no packet is associated with such ID");
+        /*/// <summary>Allows registering for a custom action under a net ID.</summary>
+        /// <param name="callback"></param>
+        /// <returns></returns>
+        public static int RegisterCustomID(Action<BinaryReader, int> callback, string? debugName) {
+
+        }*/
+        internal static void HandleModRecievedPacket(BinaryReader reader, int fromWho) {
+            int messageID = reader.ReadByte();
+            if (netMessagesHandlers.TryGetValue(messageID, out var value))
+                value.Handle(reader, fromWho);
+            else
+                EndlessEscapade.Instance.Logger.Warn($"A message of type {messageID} has been recieved but no packet is associated with such ID");
         }
-    }
-
-    public override void Load() {
-        AutoloadNetMessages();
-    }
-
-    private void AutoloadNetMessages() {
-        const BindingFlags FlagsStatic = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static;
         
-        var methodPackageHandlers = new Dictionary<Type, List<MethodInfo>>();
-        
-        foreach (var type in AssemblyManager.GetLoadableTypes(Mod.Code).OrderBy(t => t.MetadataToken)) {
-            if (type.IsGenericType) {
-                continue;
-            }
+        public override void Load() {
+            base.Load();
 
-            foreach (var method in type.GetMethods(FlagsStatic)) {
-                if (method.GetParameters() is not { Length: 1 } parameters || method.ReturnType != typeof(void) || method.GetCustomAttribute<MessageHandlerAttribute>() is not { } attribute) {
+            Stopwatch sw = Stopwatch.StartNew();
+            AutoloadNetMessages();
+            sw.Stop();
+            Mod.Logger.Info($"Net messages loading time: {sw.Elapsed.TotalMilliseconds}ms");
+        }
+
+        void AutoloadNetMessages() {
+            Dictionary<Type, List<MethodInfo>> methodPackageHandlers = new();
+            const BindingFlags FlagsStatic = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static;
+            foreach (Type type in AssemblyManager.GetLoadableTypes(Mod.Code).OrderBy(t=>t.MetadataToken)) {
+                // get listener methods
+                // specifically methods from generic types cannot be invoked without a type arg
+                if (type.IsGenericType)
                     continue;
+                foreach (MethodInfo method in type.GetMethods(FlagsStatic)) {
+                    if (method.GetParameters() is not { Length: 1 } parameters || method.ReturnType != typeof(void) || method.GetCustomAttribute<MessageHandlerAttribute>() is not { } attribute)
+                        continue;
+                    if (parameters[0].ParameterType != attribute.TargetMessageType) {
+                        Mod.Logger.Error($"{nameof(NetSystem)}: Method {type.FullName}::{method.Name} cannot be registered for listening to packets because the method's parameter is not of type {attribute.TargetMessageType}");
+                        continue;
+                    }
+                    if (!methodPackageHandlers.TryGetValue(attribute.TargetMessageType, out var list))
+                        methodPackageHandlers[attribute.TargetMessageType] = list = new();
+                    list.Add(method);
+
                 }
 
-                if (parameters[0].ParameterType != attribute.TargetMessageType) {
-                    Mod.Logger.Error(
-                        $"{nameof(NetSystem)}: Method {type.FullName}::{method.Name} cannot be registered for listening to packets because the method's parameter is not of type {attribute.TargetMessageType}"
-                    );
+                // create the messages
+                Type netMessageGeneric = typeof(INetMessage<>).MakeGenericType(type);
+                if (type.IsAbstract || !type.IsAssignableTo(netMessageGeneric))
                     continue;
-                }
 
-<<<<<<< HEAD
-                if (!methodPackageHandlers.TryGetValue(attribute.TargetMessageType, out var list)) {
-                    methodPackageHandlers[attribute.TargetMessageType] = list = new List<MethodInfo>();
-                }
-
-                list.Add(method);
-            }
-
-            // create the messages
-            var netMessageGeneric = typeof(INetMessage<>).MakeGenericType(type);
-            if (type.IsAbstract || !type.IsAssignableTo(netMessageGeneric)) {
-                continue;
-=======
                 // value types dont require a ctor
                 if (!(type.IsValueType || type.GetConstructor(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance, Type.EmptyTypes) != null))
                     continue;
@@ -96,70 +89,42 @@ public class NetSystem : ModSystem
                 Type actionType = typeof(Action<>).MakeGenericType(messageType);
                 foreach (MethodInfo handler in handlers)
                     onRecieveEvent.AddEventHandler(null, handler.CreateDelegate(messageType));
->>>>>>> 16c34049c6f66190f06d86ba3d55a46a5cc023b0
             }
-
-            // value types dont require a ctor
-            if (!(type.IsValueType || type.GetConstructor(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance, Type.EmptyTypes) != null)) {
-                continue;
-            }
-
-            var instance = Activator.CreateInstance(type)!;
-            if (!type.IsValueType) // structs cannot be fetched by ContentInstance.GetInstance
-            {
-                ContentInstance.Register(instance);
-            }
-
-            var genericMessageType = typeof(Message<>).MakeGenericType(type);
-            genericMessageType.GetMethod("Load", FlagsStatic)!.Invoke(null, new[] { instance });
-            var id = (int)genericMessageType.GetProperty("ID", FlagsStatic)!.GetValue(null)!;
-
-            netMessagesHandlers[id] = (IMessageHandler)Activator.CreateInstance(typeof(Message<>).MakeGenericType(type))!;
         }
 
-        foreach ((var messageType, var handlers) in methodPackageHandlers) {
-            var onRecieveEvent = typeof(Message<>).MakeGenericType(messageType).GetEvent("OnRecieve", FlagsStatic)!;
-            var actionType = typeof(Action<>).MakeGenericType(messageType);
-            foreach (var handler in handlers) {
-                onRecieveEvent.AddEventHandler(null, handler.CreateDelegate(messageType));
+        class CustomIDMessageHandler : IMessageHandler
+        {
+            public object? PacketInstance => null;
+
+            public string? DebugName { get; set; }
+
+            Action<BinaryReader, int> callback;
+            public CustomIDMessageHandler(Action<BinaryReader, int> callback, string? debugName = null) {
+                DebugName = debugName;
+                this.callback = callback;
+            }
+            public void Handle(BinaryReader reader, int fromWho) {
+                callback(reader, fromWho);
             }
         }
     }
 
-    private class CustomIDMessageHandler : IMessageHandler
+    record struct TestMessage(int Data) : INetMessage<TestMessage>
     {
-        private readonly Action<BinaryReader, int> callback;
-
-        public CustomIDMessageHandler(Action<BinaryReader, int> callback, string? debugName = null) {
-            DebugName = debugName;
-            this.callback = callback;
+        public readonly TestMessage Read(BinaryReader reader) {
+            return new(reader.ReadInt32());
         }
 
-        public object? PacketInstance => null;
-
-        public string? DebugName { get; }
-
-        public void Handle(BinaryReader reader, int fromWho) {
-            callback(reader, fromWho);
+        public readonly void Write(BinaryWriter writer) {
+            writer.Write(Data);
         }
     }
-}
 
-internal record struct TestMessage(int Data) : INetMessage<TestMessage>
-{
-    public readonly TestMessage Read(BinaryReader reader) {
-        return new TestMessage(reader.ReadInt32());
+    /// <summary>Reserved for internal impl details. Do not use.</summary>
+    internal interface IMessageHandler
+    {
+        object? PacketInstance { get; }
+        string? DebugName { get; }
+        void Handle(BinaryReader reader, int fromWho);
     }
-
-    public readonly void Write(BinaryWriter writer) {
-        writer.Write(Data);
-    }
-}
-
-/// <summary>Reserved for internal impl details. Do not use.</summary>
-internal interface IMessageHandler
-{
-    object? PacketInstance { get; }
-    string? DebugName { get; }
-    void Handle(BinaryReader reader, int fromWho);
 }
