8365c6aab0d908df16f60dd4420dcc70b850e688
shnakamura <joaovtnakamura@gmail.com>
2023-08-20 20:46:54 -0300

    Shifts focus to shipyard

--- EndlessEscapade.cs
+++ EndlessEscapade.cs
@@ -1,5 +1,4 @@
-﻿using EndlessEscapade.Common.Systems.Net;
-using System.IO;
+﻿using System.IO;
 using Terraria.ModLoader;
 
 namespace EndlessEscapade;
@@ -7,8 +6,4 @@ namespace EndlessEscapade;
 public class EndlessEscapade : Mod
 {
     public static EndlessEscapade Instance => ModContent.GetInstance<EndlessEscapade>();
-
-    public override void HandlePacket(BinaryReader reader, int whoAmI) {
-        NetSystem.HandleModRecievedPacket(reader, whoAmI);
-    }
 }
