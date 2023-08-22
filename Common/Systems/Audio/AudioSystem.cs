cf7f4bae76ddc7644e0b32580436bde379d85f23
LoLXD8783 <56768047+LoLXD8783@users.noreply.github.com>
2023-08-21 21:50:31 -0600

    Re-add vanilla low pass filter

--- Common/Systems/Audio/AudioSystem.cs
+++ Common/Systems/Audio/AudioSystem.cs
@@ -1,22 +1,30 @@
 ﻿using System.Collections.Immutable;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
 using EndlessEscapade.Common.Systems.Audio.Filters;
 using EndlessEscapade.Utilities;
+using FAudioINTERNAL;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using ReLogic.Utilities;
 using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
-
+using Terraria.ModLoader.IO;
+#nullable enable
 namespace EndlessEscapade.Common.Systems.Audio;
 
 [Autoload(Side = ModSide.Client)]
 public class AudioSystem : ModSystem
 {
-    private static readonly FieldInfo? trackedSoundsField = typeof(SoundPlayer).GetField("_trackedSounds", ReflectionUtils.PrivateInstanceFlags);
-    private static readonly FieldInfo? soundInstanceField = typeof(ASoundEffectBasedAudioTrack).GetField("_soundEffectInstance", ReflectionUtils.PrivateInstanceFlags);
-    
+    private static readonly FieldInfo trackedSoundsField = typeof(SoundPlayer).GetField("_trackedSounds", ReflectionUtils.PrivateInstanceFlags)!;
+    private static readonly FieldInfo soundInstanceField = typeof(ASoundEffectBasedAudioTrack).GetField("_soundEffectInstance", ReflectionUtils.PrivateInstanceFlags)!;
+    private static readonly FieldInfo cueInstanceField = typeof(CueAudioTrack).GetField("_cue", ReflectionUtils.PrivateInstanceFlags)!;
+
+    /// <summary>A value that indicates if this system loaded succesfully and is supported.</summary>
+    public static bool IsSupported { get; private set; }
+
     public static readonly ImmutableArray<SoundStyle> IgnoredSounds = ImmutableArray.Create(
         SoundID.MenuClose,
         SoundID.MenuOpen,
@@ -36,6 +44,9 @@ public class AudioSystem : ModSystem
     public static void ApplyParameters(SoundEffectInstance instance, AudioParameters parameters) {
         LowPassSystem.ApplyParameters(instance, parameters);
     }
+    internal static void ApplyParameters(Cue instance, AudioParameters parameters) {
+        LowPassSystem.ApplyParameters(instance, parameters);
+    }
 
     public override bool IsLoadingEnabled(Mod mod) {
         return SoundEngine.IsAudioSupported;
@@ -49,15 +60,34 @@ public class AudioSystem : ModSystem
 
         On_SoundEngine.PlaySound_refSoundStyle_Nullable1_SoundUpdateCallback += SoundEnginePlayHook;
         On_SoundEngine.Update += SoundEngineUpdateHook;
-        
+
         On_ASoundEffectBasedAudioTrack.Play += AudioTrackPlayHook;
         On_ASoundEffectBasedAudioTrack.Update += AudioTrackUpdateHook;
+
+        On_CueAudioTrack.Play += On_CueAudioTrack_Play;
+        On_CueAudioTrack.Update += On_CueAudioTrack_Update; ;
+        IsSupported = true;
+    }
+
+    private void On_CueAudioTrack_Update(On_CueAudioTrack.orig_Update orig, CueAudioTrack self) {
+        orig(self);
+
+        Cue cue = (Cue)cueInstanceField.GetValue(self)!;
+        ApplyParameters(cue, MusicParameters);
+    }
+
+    private static unsafe void On_CueAudioTrack_Play(On_CueAudioTrack.orig_Play orig, CueAudioTrack self) {
+        orig(self);
+
+        Cue cue = (Cue)cueInstanceField.GetValue(self)!;
+        //Thread.Sleep(100); // apperently theres a data race
+        ApplyParameters(cue, MusicParameters);
     }
 
     private static SlotId SoundEnginePlayHook(On_SoundEngine.orig_PlaySound_refSoundStyle_Nullable1_SoundUpdateCallback orig, ref SoundStyle style, Vector2? position, SoundUpdateCallback callback) {
         var slot = orig(ref style, position, callback);
 
-        if (IgnoredSounds.Contains(style) || !SoundEngine.TryGetActiveSound(slot, out var result) || result.Sound.IsDisposed) {
+        if (IgnoredSounds.Contains(style) || !SoundEngine.TryGetActiveSound(slot, out var result) || result.Sound?.IsDisposed is not false) {
             return slot;
         }
 
@@ -69,13 +99,13 @@ public class AudioSystem : ModSystem
     private static void SoundEngineUpdateHook(On_SoundEngine.orig_Update orig) {
         orig();
 
-        var value = (SlotVector<ActiveSound>)trackedSoundsField.GetValue(SoundEngine.SoundPlayer);
+        var value = (SlotVector<ActiveSound>)trackedSoundsField.GetValue(SoundEngine.SoundPlayer)!;
 
         foreach (var item in value) {
             var sound = item.Value;
             var instance = sound.Sound;
 
-            if (IgnoredSounds.Contains(sound.Style) || !sound.IsPlaying || instance.IsDisposed) {
+            if (IgnoredSounds.Contains(sound.Style) || !sound.IsPlaying || instance?.IsDisposed is not false) {
                 continue;
             }
 
@@ -84,22 +114,22 @@ public class AudioSystem : ModSystem
 
         SoundParameters = default;
     }
-    
+
     private static void AudioTrackPlayHook(On_ASoundEffectBasedAudioTrack.orig_Play orig, ASoundEffectBasedAudioTrack self) {
         orig(self);
 
-        var instance = (DynamicSoundEffectInstance)soundInstanceField.GetValue(self);
-        
+        var instance = (DynamicSoundEffectInstance)soundInstanceField.GetValue(self)!;
+
         ApplyParameters(instance, MusicParameters);
     }
-    
+
     private static void AudioTrackUpdateHook(On_ASoundEffectBasedAudioTrack.orig_Update orig, ASoundEffectBasedAudioTrack self) {
         orig(self);
-        
-        var instance = (DynamicSoundEffectInstance)soundInstanceField.GetValue(self);
-        
+
+        var instance = (DynamicSoundEffectInstance)soundInstanceField.GetValue(self)!;
+
         ApplyParameters(instance, MusicParameters);
-        
+
         MusicParameters = default;
     }
 }
--- Common/Systems/Audio/Filters/LowPassSystem.cs
+++ Common/Systems/Audio/Filters/LowPassSystem.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using EndlessEscapade.Common.Config;
 using EndlessEscapade.Utilities;
+using FAudioINTERNAL;
 using Microsoft.Xna.Framework.Audio;
 using Terraria.Audio;
 using Terraria.ModLoader;
@@ -11,6 +14,7 @@ namespace EndlessEscapade.Common.Systems.Audio.Filters;
 public class LowPassSystem : ModSystem
 {
     private static readonly Action<SoundEffectInstance, float> lowPassAction;
+    private static readonly FieldInfo cueHandleInstanceField = typeof(Cue).GetField("handle", ReflectionUtils.PrivateInstanceFlags)!;
 
     static LowPassSystem() {
         var type = typeof(SoundEffectInstance);
@@ -30,4 +34,53 @@ public class LowPassSystem : ModSystem
 
         lowPassAction.Invoke(instance, intensity);
     }
+
+    internal static unsafe void ApplyParameters(Cue cue, AudioParameters parameters) {
+        nint handle = (nint)cueHandleInstanceField.GetValue(cue)!;
+
+        FACTCue* cuePtr = (FACTCue*)handle;
+
+        FAudio.FAudioFilterParameters filterParameters = new() {
+            Frequency = 1f - parameters.LowPass * 0.9f,
+            OneOverQ = 1,
+            Type = FAudio.FAudioFilterType.FAudioLowPassFilter
+        };
+        if (cuePtr->simpleWave != null && cuePtr->simpleWave->voice != null) {
+            FAudioVoice* voice = cuePtr->simpleWave->voice;
+            //FAudio.FAudioVoice_GetVoiceDetails((nint)voice, out FAudio.FAudioVoiceDetails details);
+            FAudio.FAudioVoice_SetFilterParameters((nint)voice, ref filterParameters, 0u);
+        }
+        else if (cuePtr->playingSound != null) {
+            FACTSound* factSound = cuePtr->playingSound->sound;
+            int count = factSound->trackCount;
+            for (int i = 0; i < count; i++) {
+                ref var sound = ref cuePtr->playingSound;
+                ref var tracks = ref sound->tracks[i];
+                ref var wave1 = ref tracks.activeWave;
+                FACTWave* wave2 = wave1.wave;
+                if (wave2 == null) // seems like there's a data race where this may ocasionally be null
+                    continue; 
+                FAudioVoice* voice = wave2->voice;
+                Marshal.ThrowExceptionForHR((int)FAudio.FAudioVoice_SetFilterParameters((nint)voice, ref filterParameters, 0u));
+            }
+        }
+        //if (cuePtr->simpleWave != null && cuePtr->simpleWave->voice != null) {
+        //    FAudioVoice* voice = cuePtr->simpleWave->voice;
+
+        //    FAudio.FAudioVoice_SetFilterParameters((nint)voice, ref filterParameters, 0);
+        //}
+        //else if (cuePtr->playingSound != null && cuePtr->playingSound->sound != null) {
+        //    FACTSound* factsound = cuePtr->playingSound->sound;
+        //    int count = factsound->trackCount;
+        //    for (int i = 0; i < count; i++) {
+        //        ref var sound = ref cuePtr->playingSound;
+        //        ref var tracks = ref sound->tracks[i];
+        //        ref var wave1 = ref tracks.activeWave;
+        //        FACTWave* wave2 = wave1.wave;
+        //        FAudioVoice* voice = wave2->voice;
+        //        Marshal.ThrowExceptionForHR((int)FAudio.FAudioVoice_SetFilterParameters((nint)voice, ref filterParameters, 0u));
+        //    }
+        //}
+    }
+
 }
