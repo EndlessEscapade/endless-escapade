a663a51af42ea7e0cf7921d687b20d86954af925
shnakamura <joaovtnakamura@gmail.com>
2023-12-01 12:46:41 -0300

    Storm Knight

---
+++ Content/Items/Lythen/StormKnightBreastplate.cs
@@ -0,0 +1,12 @@
+using Terraria.ModLoader;
+
+namespace EndlessEscapade.Content.Items.Lythen;
+
+[AutoloadEquip(EquipType.Body)]
+public class StormKnightBreastplate : ModItem
+{
+    public override void SetDefaults() {
+        Item.width = 20;
+        Item.height = 20;
+    }
+}
---
+++ Content/Items/Lythen/StormKnightLeggings.cs
@@ -0,0 +1,12 @@
+using Terraria.ModLoader;
+
+namespace EndlessEscapade.Content.Items.Lythen;
+
+[AutoloadEquip(EquipType.Legs)]
+public class StormKnightLeggings : ModItem
+{
+    public override void SetDefaults() {
+        Item.width = 20;
+        Item.height = 20;
+    }
+}
