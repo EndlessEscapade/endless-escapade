8867f5fdb4ba86e7952b35c6e2927405b459d62b
shnakamura <joaovtnakamura@gmail.com>
2024-01-07 02:02:40 -0300

    EC progress

--- Common/EC/Component.cs
+++ Common/EC/Component.cs
@@ -2,5 +2,13 @@ namespace EndlessEscapade.Common.EC;
 
 public abstract class Component
 {
+    public Entity Parent { get; internal set; }
     
+    public virtual void Update() { }
+    
+    public virtual void Draw() { }
+    
+    public virtual void Spawn() { }
+    
+    public virtual void Death() { }
 }
--- Common/EC/Entity.cs
+++ Common/EC/Entity.cs
@@ -1,6 +1,8 @@
+using System.Collections.Generic;
+
 namespace EndlessEscapade.Common.EC;
 
 public sealed class Entity
 {
-    
+    public readonly List<Component> Components = new();
 }
