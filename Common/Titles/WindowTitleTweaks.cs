8648a8da2f2705a84408ef41f0436049fae0531f
shnakamura <joaovtnakamura@gmail.com>
2024-01-11 11:23:53 -0300

    Makes a patch to properly set custom window titles

--- Common/Titles/WindowTitleTweaks.cs
+++ Common/Titles/WindowTitleTweaks.cs
@@ -1,3 +1,6 @@
+using System;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
 using Terraria;
 using Terraria.Localization;
 using Terraria.ModLoader;
@@ -7,14 +10,34 @@ namespace EndlessEscapade.Common.Titles;
 [Autoload(Side = ModSide.Client)]
 public sealed class WindowTitleTweaks : ModSystem
 {
-    // TODO: Make the title change when a language is set.
-    public override void PostSetupContent() {
+    public override void Load() {
+        IL_Main.DrawMenu += DrawMenuPatch;
+    }
+
+    public override void Unload() {
+        Main.changeTheTitle = true;
+    }
+
+    private static void SetTitle() {
         const int GameTitleCount = 25;
         
+        Main.changeTheTitle = false;
         Main.instance.Window.Title = Language.GetTextValue("Mods.EndlessEscapade.GameTitle." + Main.rand.Next(GameTitleCount));
     }
     
-    public override void Unload() {
-        Main.changeTheTitle = true;
+    private void DrawMenuPatch(ILContext il) {
+        try {
+            var c = new ILCursor(il);
+
+            if (!c.TryGotoNext(MoveType.After, i => i.MatchLdcI4(1))) {
+                EndlessEscapade.Instance.Logger.Warn($"{nameof(WindowTitleTweaks)} disabled: Failed to match IL.");
+                return;
+            }
+
+            c.EmitDelegate(SetTitle);
+        }
+        catch (Exception exception) {
+            MonoModHooks.DumpIL(Mod, il);
+        }
     }
 }
