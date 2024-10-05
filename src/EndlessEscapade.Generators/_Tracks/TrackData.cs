c519fceebee8c91fca85ff3f63140e2edb632cdc
LolXD8783 <lelomlalol@gmail.com>
2024-10-05 11:45:46 -0600

    Add Microsoft.Bcl.HashCode and improve TrackGenerator cache

--- src/EndlessEscapade.Generators/_Tracks/TrackData.cs
+++ src/EndlessEscapade.Generators/_Tracks/TrackData.cs
@@ -1,8 +1,10 @@
 using Newtonsoft.Json;
+using System;
+using System.Linq;
 
 namespace EndlessEscapade.Generators
 {
-	public sealed class TrackData
+	public sealed class TrackData : IEquatable<TrackData>
 	{
 		[JsonRequired]
 		public string SoundPath;
@@ -13,5 +15,22 @@ namespace EndlessEscapade.Generators
 		public float StepIn = 0.05f;
 
 		public float StepOut = 0.05f;
-	}
+
+        public bool Equals(TrackData other)
+        {
+			return other != null 
+				&& other.SoundPath == SoundPath 
+				&& other.Signals.AsSpan().SequenceEqual(other.Signals)
+				&& other.StepIn == StepIn
+				&& other.StepOut == StepOut;
+        }
+		public override bool Equals(object obj)
+		{
+			return Equals(obj as TrackData);
+		}
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(SoundPath, Signals.GetHashCode(), StepIn, StepOut);
+        }
+    }
 }
